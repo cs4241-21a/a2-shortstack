72240538a7da6d4843681cb53fec0d55463858a7
Nathan Klingensmith <njklingensmith@wpi.edu>
2021-08-30 16:42:29 -0400

    Fix bugs with updating data

--- server.improved.js
+++ server.improved.js
@@ -13,31 +13,34 @@ http.createServer((req, res) => {
   if (req.method === 'GET') {
     GET(req, res);
   } else if (req.method === 'POST') {
-    POST (req, res);
+    POST(req, res);
   }
 }).listen(process.env.PORT || port);
 
 const GET = (req, res) => sendFile(res, dir + (routes[req.url] || req.url));
 
 const POST = (req, res) => {
-  let newData = {};
-  req.on( 'data', data => newData = JSON.parse(data));
-  req.on('end', () => resolve(res, 200, JSON.stringify(updateData(newData))));
+  let data = {};
+  req.on('data', async newData => {
+    data = JSON.stringify(await updateData(JSON.parse(newData)));
+  });
+  req.on('end', () => resolve(res, 200, data));
 };
 
 const sendFile = (res, path) => {
   fs.readFile(path, (err, data) =>
-      resolve(res, err ? 404: 200, err ? "File not found:" : data));
+      resolve(res, err ? 404 : 200, err ? "File not found:" : data));
 };
 
+// updates data by appending a new data object, returns updated data
+const updateData = async (newData) => {
+  const data = (JSON.parse(await fs.readFileSync(dataPath))).concat([newData]);
+  fs.writeFile(dataPath, JSON.stringify(data), () => null);
+  return data;
+};
+
+// resolves request with code and response.body data
 const resolve = (res, code, data = null) => {
   res.writeHead(code);
   res.end(data);
-};
-
-
-const updateData = async (newData) => {
-  const data = require(dataPath).concat([newData]);
-  fs.writeFile(dataPath, JSON.stringify(data), console.log);
-  return data;
 };
