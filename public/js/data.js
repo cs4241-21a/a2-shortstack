31a8bf582ba15644e19a1d35b5a96bc459f519bc
Nathan Klingensmith <njklingensmith@wpi.edu>
2021-08-30 15:36:28 -0400

    Add data file, improve server.js

---
+++ public/js/data.js
@@ -0,0 +1,10 @@
+const data = [
+    {
+        username: 'Paradoxdotexe',
+        content: 'Hello world!',
+        submitted: '',
+        admin: true
+    }
+];
+
+export default data;
--- server.improved.js
+++ server.improved.js
@@ -1,72 +1,48 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+import data from "./public/js/data";
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+const http = require('http');
+const fs   = require('fs');
+const mime = require('mime');
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+const dir = './public';
+const dataPath = `{dir}/js/data`;
+const port = 3000;
+const routes = [
+  '/': '/index.html'
+];
+
+http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    GET(req, res);
+  } else if (req.method === 'POST') {
+    POST (req, res);
   }
-})
+}).listen(process.env.PORT || port);
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+const GET = (req, res) => sendFile(res, routes[req.url] || req.url);
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
-  }
-}
+const POST = (req, res) => {
+  let newData = {};
+  req.on( 'data', data => newData = JSON.parse(data));
 
-const handlePost = function( request, response ) {
-  let dataString = ''
+  req.on('end', () => {
+    console.log(newData);
+    addData(newData);
+    resolve(res, 200);
+  });
+};
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
+const sendFile = (res, path) => {
+  fs.readFile(path, (err, data) => resolve(res, err ? 404: 200, err ? "File not found:" : data));
+};
 
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+const resolve = (res, code, data = null) => {
+  res.writeHead(code);
+  res.end(data);
+};
 
-    // ... do something with the data here!!!
-
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
-  })
-}
-
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
-
-   fs.readFile( filename, function( err, content ) {
-
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
-
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
-
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
-}
-
-server.listen( process.env.PORT || port )
+const addData = (newData) => {
+  import(dataPath).then(data => {
+    fs.writeFile(dataPath, data + newData);
+  });
+};
