ee003d54cf73a4a33c9700f9e95f127fdf5020a1
Nathan Klingensmith <njklingensmith@wpi.edu>
2021-08-31 08:53:42 -0400

    Add index.css, add helpers.js

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -1,7 +1,7 @@
-export const renderTemplate = (template, id, data) =>
+const renderTemplate = (template, id, data) =>
     document.getElementById(id).innerHTML = Mustache.render(template, { data });
 
-export const parseUsername = () => {
+const parseUsername = () => {
     const usernameMatch = window.location.search.match('username=(.+?)(?:$|&)');
     return usernameMatch ? usernameMatch[1] : null;
 }
