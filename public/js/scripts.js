c7ef286a69b342786a893caf25ce8b1bfeb321c2
Federico Galbiati <fgalbiati@wpi.edu>
2021-08-31 01:58:27 +0000

    Started Implementing JS + forms

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,42 @@
-// Add some Javascript code here, to run on the front end.
+  const getRequest = function(endpoint, data, callback) {
+  fetch( endpoint, {
+    method:'GET',
+    data 
+  })
+  .then( callback(response) )
+  return false
+}
 
-console.log("Welcome to assignment 2!")
+function login() {
+  getRequest('/login', {
+    username,
+    password
+  }, (response) => {
+    if (response.success) {
+      loginSuccess()
+    } else {
+      loginFailed()
+    }
+  })
+}
+
+function loginSuccess() {
+  document.querySelector( '#loginform' ).style.display = 'none'
+  els = document.querySelectorAll( '.logged-in' )
+  els.forEach(el => el.style.display = 'block')
+}
+
+function loginFailed() {
+  document.querySelector('#error-box').innerText = "Error! Wrong username or password!"
+}
+
+function getMatches() {
+  document.querySelector( '#' )
+}
+
+window.onload = function() {
+    document.querySelector( '#login-button' ).onclick = (e) => {
+      e.preventDefault()
+      login()
+    }
+  }
