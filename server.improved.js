a7abe1fc37c16d2f1f27084d4c28d8527807e514
chrisvieira <christophervieira1199@gmail.com>
2021-09-01 21:55:55 -0400

    Works based on new schema of SA/TA

--- server.improved.js
+++ server.improved.js
@@ -1,256 +1,177 @@
-const { report } = require('process')
+const { report } = require("process");
 
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require("http"),
+  fs = require("fs"),
+  // IMPORTANT: you must run `npm install` in the directory for this assignment
+  // to install the mime library used in the following line of code
+  mime = require("mime"),
+  dir = "public/",
+  port = 3000;
 
-const appdata = [
-   
-]
+const appdata = [];
 
-let id_counter = 0
+let id_counter = 0;
 
-
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+const server = http.createServer(function (request, response) {
+  if (request.method === "GET") {
+    handleGet(request, response);
+  } else if (request.method === "POST") {
+    handlePost(request, response);
   }
-})
+});
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+const handleGet = function (request, response) {
+  const filename = dir + request.url.slice(1);
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  } else if( request.url === '/initializeData' ) {
-    // console.log("Attempting initialize data")
-    getInitialDataList( response )
-  }else{
-    sendFile( response, filename )
+  if (request.url === "/") {
+    sendFile(response, "public/index.html");
+  } else if (request.url === "/initializeData") {
+    getInitialDataList(response);
+  } else {
+    sendFile(response, filename);
   }
-}
+};
 
-
-const handlePost = function( request, response ) {
-  if( request.url === '/submit' ) {
-    // console.log("Attempting add data")
-    handleAddition( request, response )
-  } else if( request.url === '/deleteEntry' ) {
-    handleDeletion(request, response)
-    // console.log("Attempting delete data")
-  } else if( request.url === '/updateEntry' ) {
-    handleUpdate(request, response)
-    // console.log("Attempting delete data")
-  }else{
-    console.log("I don't know what you want me to do!")
+const handlePost = function (request, response) {
+  if (request.url === "/submit") {
+    handleAddition(request, response);
+  } else if (request.url === "/deleteEntry") {
+    handleDeletion(request, response);
+  } else if (request.url === "/updateEntry") {
+    handleUpdate(request, response);
+  } else {
+    console.log("I don't know what you want me to do!");
   }
-
-}
+};
 
 function handleAddition(request, response) {
-  let dataString = ''
+  let dataString = "";
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-      // console.log("Testing4")
-      // dataString += "Heloooooooooooooooooooooooooooooo"
-      // response.bodyUsed = true
-      // response.body = "testing111"
-      // console.log("request")
-      // console.log(request)
-  })
+  request.on("data", function (data) {
+    dataString += data;
+  });
 
-  // request.on( 'rand_test', function() {
-    // console.log("111111111111111111")
-  // })
+  request.on("end", function () {
+    console.log("\n\nNEW POST DATA REQUEST");
 
-  request.on( 'end', function() {
-    console.log("\n\nNEW POST DATA REQUEST")
-    // console.log("dataString")
-    // console.log( dataString  )
-    let dataStringParsed = JSON.parse( dataString )
-    // console.log("dataString parsed")
-    // console.log( dataStringParsed )
-    appdata.push({ 'name': dataStringParsed.yourname, "id": id_counter})
-    id_counter = id_counter + 1
-    console.log("appData")
-    console.log(appdata)
-    const string_app_data = JSON.stringify(appdata)
-    // console.log("string appData")
-    // console.log(string_app_data)
-    // let unordered_list = window.document.getElementById("trackedForm");
-    // let list_item = document.createElement("newListItem");
-    // list_item.innerText = dataString;
-    // unordered_list.appendChild(list_item);
-    
-    // ... do something with the data here!!!
+    let dataStringParsed = JSON.parse(dataString);
 
-    // const json = JSON.parse(dataString)
+    appdata.push({
+      StudentName: dataStringParsed.StudentName,
+      StudentClass: dataStringParsed.StudentClass,
+      StudentRole: dataStringParsed.StudentRole,
+      id: id_counter,
+    });
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain'})
-    // response.write(string_app_data)
+    id_counter = id_counter + 1;
 
-    // console.log("Response Object")
-    // console.log(response.bodyUsed)
-    // console.log(response.body)
+    console.log("appData");
+    console.log(appdata);
 
-    // response.bodyUsed = true
-    // response.body = string_app_data
-
-    // console.log("Response Object Post Change")
-    // console.log(response.bodyUsed)
-    // console.log(response.body)
-
-    // response
-    // .writeHead(200, {
-    //   'Content-Length': Buffer.byteLength(string_app_data),
-    //   'Content-Type': 'text/plain'
-    // })
-    // .end(string_app_data);
-    
-    // response.write(string_app_data)
-    // response.write(string_app_data)
-    // response.write(string_app_data)
-    // response.write(string_app_data)
-    // response.body = string_app_data
-    // response.end(string_app_data)
-    // response.end("Hello There")
-    // response.data = string_app_data
-    // response.sendFile = string_app_data
-    response.end(string_app_data)
-    // response.end(appdata)
-  })
-
+    const string_app_data = JSON.stringify(appdata);
+    response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+    response.end(string_app_data);
+  });
 }
 
 function handleDeletion(request, response) {
-  let dataString = ''
+  let dataString = "";
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
+  request.on("data", function (data) {
+    dataString += data;
+  });
 
-  request.on( 'end', function() {
+  request.on("end", function () {
+    console.log("\n\nNEW DELETE DATA REQUEST");
 
-    console.log("\n\nNEW DELETE DATA REQUEST")
-    // console.log("dataString")
-    // console.log( dataString  )
-    let dataStringParsed = JSON.parse( dataString )
-    // console.log("dataString parsed")
-    // console.log( dataStringParsed )
-    // appdata.push({ 'name': dataStringParsed.yourname, "id": id_counter})
+    let dataStringParsed = JSON.parse(dataString);
 
-    let item_index = -1
+    let item_index = -1;
 
     for (let i = 0; i < appdata.length; i++) {
-      // console.log("dataString.id")
-      // console.log(dataStringParsed.id)
-      // console.log("appdata[i].id")
-      // console.log(appdata[i].id)
-      if (String(appdata[i].id) === String(dataStringParsed.id)){
-        // console.log("Found it" + appdata[i].id)
-        item_index = i
-        break
-      } else {
-        // console.log("Not " + appdata[i].id)
+      if (String(appdata[i].id) === String(dataStringParsed.id)) {
+        item_index = i;
+        break;
       }
-    } 
-
-    if (item_index != -1) {
-      appdata.splice(item_index, 1)
-      // appdata.removeIndex(item_index)
     }
 
-    console.log("appData")
-    console.log(appdata)
-    const string_app_data = JSON.stringify(appdata)
+    if (item_index != -1) {
+      appdata.splice(item_index, 1);
+    }
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain'})
+    console.log("appData");
+    console.log(appdata);
+    const string_app_data = JSON.stringify(appdata);
 
-    response.end(string_app_data)
-  })
+    response.writeHead(200, "OK", { "Content-Type": "text/plain" });
 
+    response.end(string_app_data);
+  });
 }
 
 function handleUpdate(request, response) {
-  let dataString = ''
+  let dataString = "";
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
+  request.on("data", function (data) {
+    dataString += data;
+  });
 
-  request.on( 'end', function() {
+  request.on("end", function () {
+    console.log("\n\nNEW UPDATE DATA REQUEST");
 
-    console.log("\n\nNEW UPDATE DATA REQUEST")
+    let dataStringParsed = JSON.parse(dataString);
 
-    let dataStringParsed = JSON.parse( dataString )
-
-    console.log("dataStringParsed")
-    console.log(dataStringParsed)
+    console.log("dataStringParsed");
+    console.log(dataStringParsed);
 
     for (let i = 0; i < appdata.length; i++) {
-
-      if (String(appdata[i].id) === String(dataStringParsed.id)){
-        appdata[i].name = dataStringParsed.yourname
-        break
+      if (String(appdata[i].id) === String(dataStringParsed.id)) {
+        appdata[i].StudentName = dataStringParsed.StudentName;
+        appdata[i].StudentClass = dataStringParsed.StudentClass;
+        appdata[i].StudentRole = dataStringParsed.StudentRole;
+        break;
       }
-    } 
+    }
 
-    console.log("appData")
-    console.log(appdata)
-    const string_app_data = JSON.stringify(appdata)
+    console.log("appData");
+    console.log(appdata);
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain'})
+    const string_app_data = JSON.stringify(appdata);
 
-    response.end(string_app_data)
-  })
+    response.writeHead(200, "OK", { "Content-Type": "text/plain" });
 
+    response.end(string_app_data);
+  });
 }
 
-const getInitialDataList = function(response){
-  // request.on( 'end', function() {
-  //   console.log("\n\nNEW GET INITIAL DATA REQUEST")
-  //   console.log("appData")
-  //   console.log(appdata)
-  //   const string_app_data = JSON.stringify(appdata)
-  //   response.writeHead( 200, "OK", {'Content-Type': 'text/plain'})
-  //   response.end(string_app_data)
-  // })
-  console.log("\n\nNEW GET INITIAL DATA REQUEST")
-  console.log("appData")
-  console.log(appdata)
-  const string_app_data = JSON.stringify(appdata)
-  response.writeHead( 200, "OK", {'Content-Type': 'text/plain'})
-  response.end(string_app_data)
+const getInitialDataList = function (response) {
+  console.log("\n\nNEW GET INITIAL DATA REQUEST");
 
-}
+  console.log("appData");
+  console.log(appdata);
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+  const string_app_data = JSON.stringify(appdata);
 
-   fs.readFile( filename, function( err, content ) {
+  response.writeHead(200, "OK", { "Content-Type": "text/plain" });
 
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
+  response.end(string_app_data);
+};
 
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
+const sendFile = function (response, filename) {
+  const type = mime.getType(filename);
 
-     }else{
+  fs.readFile(filename, function (err, content) {
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { "Content-Type": type });
+      response.end(content);
+    } else {
+      // file not found, error code 404
+      response.writeHeader(404);
+      response.end("404 Error: File Not Found");
+    }
+  });
+};
 
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
-}
-
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port);
