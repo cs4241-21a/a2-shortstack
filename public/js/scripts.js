e92dadd40d090e78b19525251671d20a749a9018
sjordhani22 <sjordhani@wpi.edu>
2021-09-04 08:39:35 -0400

    Work on add, edit, and delete functionality. Getting console errors on FireFox

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,71 +1,190 @@
 // Add some Javascript code here, to run on the front end.
 
 function generateTable() {
-  fetch('/table', {
-    method: 'POST',
+  fetch("/table", {
+    method: "POST",
   })
-  .then (function ( response ) {
-    return response.json()
-  })
-  .then (function( json ) {
-    refreshTable(json)
-  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      refreshTable(json);
+    });
+}
+
+function resetFormToDefault() {
+  document.querySelector("#hotel-name").value = "Enter Hotel Name Here";
+  document.querySelector("#hotel-location").value = "Enter Hotel Location Here";
+  document.querySelector("#cleanliness-score").value = 5;
+  document.querySelector("#service-score").value = 5;
+  document.querySelector("#amenity-score").value = 5;
 }
 
 function refreshTable(data) {
-  let table = document.querySelector('table')
-    let thead = table.createTHead() 
-    let header_row = thead.insertRow() 
-    let table_columns = ['Hotel Name', 'Hotel Location', 'Cleanliness', 'Service', 'Amenity', 'Overall Score']
-    for (let title of table_columns) {
-      let th = document.createElement('th')
-      let header_text = document.createTextNode(title)
-      th.appendChild(header_text)
-      header_row.appendChild(th)
+  let table = document.querySelector("table");
+  if (table === null) {
+    table = document.createElement("table");
+    document.body.appendChild(table);
+  }
+  let tbody = table.createTBody();
+  let thead = table.createTHead();
+  let header_row = thead.insertRow();
+  let table_columns = [
+    "Hotel Name",
+    "Hotel Location",
+    "Cleanliness",
+    "Service",
+    "Amenity",
+    "Overall Score",
+    "Edit",
+    "Delete",
+  ];
+  for (let title of table_columns) {
+    let th = document.createElement("th");
+    let header_text = document.createTextNode(title);
+    th.appendChild(header_text);
+    header_row.appendChild(th);
+  }
+  for (let element of data) {
+    let row = tbody.insertRow();
+    for (let key of Object.keys(data[0])) {
+      let cell = row.insertCell();
+      let text = document.createTextNode(element[key]);
+      cell.appendChild(text);
     }
-    for (let element of data) {
-      let row = table.insertRow();
-      for(let key of Object.keys(data[0])) {
-        let cell = row.insertCell();
-        let text = document.createTextNode(element[key]);
-        cell.appendChild(text)
-      }
-    }
+
+    // --------------------- Edit Button Code Below ---------------------
+
+    let edit_button_cell = row.insertCell();
+    const edit_btn = document.createElement("button");
+    const edit_btn_text = "Edit";
+    edit_btn.innerHTML = edit_btn_text;
+    edit_btn.onclick = () => {
+      let original_entry = element;
+      document.querySelector("#hotel-name").value = element.hotel;
+      document.querySelector("#hotel-location").value = element.location;
+      document.querySelector("#cleanliness-score").value = element.cleanliness;
+      document.querySelector("#service-score").value = element.service;
+      document.querySelector("#amenity-score").value = element.amenity;
+      const save_btn = document.querySelector("#Submit-Button");
+      save_btn.innerHTML = "Save Changes";
+      save_btn.onclick = () => {
+        console.log("Just clicked save button");
+        const hotel_name = document.querySelector("#hotel-name"),
+          hotel_location = document.querySelector("#hotel-location"),
+          cleanliness_score = document.querySelector("#cleanliness-score"),
+          service_score = document.querySelector("#service-score"),
+          amenity_score = document.querySelector("#amenity-score"),
+          json = [
+            {
+              hotel: original_entry.hotel,
+              location: original_entry.location,
+              cleanliness: original_entry.cleanliness,
+              service: original_entry.service,
+              amenity: original_entry.amenity,
+            },
+            {
+              hotel: hotel_name.value,
+              location: hotel_location.value,
+              cleanliness: Number(cleanliness_score.value),
+              service: Number(service_score.value),
+              amenity: Number(amenity_score.value),
+            },
+          ],
+          body = JSON.stringify(json);
+        console.log(hotel_name.value);
+        fetch("/edit", {
+          method: "POST",
+          body,
+        })
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (json) {
+            let table = document.querySelector("table");
+            table.remove();
+            refreshTable(json);
+            resetFormToDefault();
+            save_btn.innerHTML = "Submit Changes";
+            save_btn.onclick = submit;
+          });
+      };
+    };
+    edit_button_cell.appendChild(edit_btn);
+
+    // --------------------- Delete Button Code Below ---------------------
+
+    let delete_button_cell = row.insertCell();
+    const delete_btn = document.createElement("button");
+    const delete_btn_text = "Delete";
+    delete_btn.innerHTML = delete_btn_text;
+    delete_btn.onclick = () => {
+      resetFormToDefault();
+      const form_btn = document.querySelector("#Submit-Button");
+      form_btn.onclick = submit;
+      form_btn.innerHTML = "Submit Review";
+      const json = {
+          hotel: element.hotel,
+          location: element.location,
+          cleanliness: element.cleanliness,
+          service: element.service,
+          amenity: element.amenity,
+        },
+        body = JSON.stringify(json);
+      fetch("/delete", {
+        method: "POST",
+        body,
+      })
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (json) {
+          let table = document.querySelector("table");
+          table.remove();
+          refreshTable(json);
+        });
+    };
+    delete_button_cell.appendChild(delete_btn);
+  }
 }
 
 const submit = function (e) {
   // prevent default form action from being carried out
   e.preventDefault();
 
-  const hotel_name = document.querySelector('#hotel-name'),
-    hotel_location = document.querySelector('#hotel-location'),
-    cleanliness_score = document.querySelector('#cleanliness-score'),
-    service_score = document.querySelector('#service-score'),
-    amenity_score = document.querySelector('#amenity-score'),
+  const hotel_name = document.querySelector("#hotel-name"),
+    hotel_location = document.querySelector("#hotel-location"),
+    cleanliness_score = document.querySelector("#cleanliness-score"),
+    service_score = document.querySelector("#service-score"),
+    amenity_score = document.querySelector("#amenity-score"),
     json = {
-      hotelname: hotel_name.value,
-      hotellocattion: hotel_location.value,
-      cleanliness: cleanliness_score.value,
-      service: service_score.value,
-      amenity: amenity_score.value
+      hotel: hotel_name.value,
+      location: hotel_location.value,
+      cleanliness: Number(cleanliness_score.value),
+      service: Number(service_score.value),
+      amenity: Number(amenity_score.value),
     },
     body = JSON.stringify(json);
 
-  fetch('/submit', {
-    method: 'POST',
+  fetch("/submit", {
+    method: "POST",
     body,
-  }).then(function (response) {
-    return response.json()
   })
-  .then(function( json ) {
-    console.log(json)
-  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      let table = document.querySelector("table");
+      table.remove();
+      refreshTable(json);
+      resetFormToDefault();
+    });
 
   return false;
-}
+};
 
 window.onload = function () {
-  generateTable()
-  const button = document.querySelector('button');
+  generateTable();
+  const button = document.querySelector("#Submit-Button");
   button.onclick = submit;
 };
--- server.improved.js
+++ server.improved.js
@@ -1,85 +1,163 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require('http'),
+  fs = require('fs'),
+  // IMPORTANT: you must run `npm install` in the directory for this assignment
+  // to install the mime library used in the following line of code
+  mime = require('mime'),
+  dir = 'public/',
+  port = 3000;
 
 const appdata = [
-  { 'hotel-name': 'Best Resort', 'location': 'Miami', 'cleanliness': 9, 'service': 8, 'amenity': 7, 'overall-experience':8},
-  { 'hotel-name': 'Roadside Inn', 'location': 'Boston', 'cleanliness': 5, 'service': 5, 'amenity': 5, 'overall-experience':5}
-]
+  {
+    'hotel': 'Best Resort',
+    'location': 'Miami',
+    'cleanliness': 9,
+    'service': 8,
+    'amenity': 7,
+    'overallexperience': 8
+  },
+  {
+    'hotel': 'Roadside Inn',
+    'location': 'Boston',
+    'cleanliness': 5,
+    'service': 5,
+    'amenity': 5,
+    'overallexperience': 5
+  },
+];
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+const server = http.createServer(function (request, response) {
+  if (request.method === 'GET') {
+    handleGet(request, response);
+  } else if (request.method === 'POST') {
+    handlePost(request, response);
   }
-})
+});
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+const handleGet = function (request, response) {
+  const filename = dir + request.url.slice(1);
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
+  if (request.url === '/') {
+    sendFile(response, 'public/index.html');
+  } else {
+    sendFile(response, filename);
   }
-}
+};
 
-const getOverallScore = function (cleanlinessScore, serviceScore, amenityScore) {
+const getOverallScore = function (
+  cleanlinessScore,
+  serviceScore,
+  amenityScore
+) {
   let sum = cleanlinessScore + serviceScore + amenityScore;
-  return sum / 3
-}
+  return Math.round((sum / 3) * 10) / 10;
+};
 
-const handlePost = function( request, response ) {
-  let dataString = ''
+const handlePost = function (request, response) {
+  let dataString = '';
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
+  request.on('data', function (data) {
+    dataString += data;
+  });
 
-  request.on( 'end', function() {
-    if( request.url === '/table') {
-      console.log('Inside post for getting table')
-      response.writeHead( 200, 'OK', {'Content-Type': 'text/plain' })   
-      response.end(JSON.stringify( appdata ))
-    } else {
-    const json = JSON.parse( dataString )
-    const overallScore = getOverallScore(Number(json.cleanliness), Number(json.service), Number(json.amenity))
-    appdata.push({'hotel-name': json.hotelname, 'location': json.hotellocation, 'cleanliness': json.cleanliness, 'service': json.service, 'amenity': json.amenity, 'overall-experience':overallScore})
-    // ... do something with the data here!!!
-    console.log( json )
-    console.log(`Here is the overall score: ${overallScore}`)
-    response.writeHead( 200, 'OK', {'Content-Type': 'text/plain' })
-    response.end(JSON.stringify( json ))
+  request.on('end', function () {
+    switch (request.url) {
+      case '/table':
+        {
+          response.writeHead(200, 'OK', { 'Content-Type': 'text/plain' });
+          response.end(JSON.stringify(appdata));
+        }
+        break;
+
+      case '/submit':
+        {
+          const json = JSON.parse(dataString);
+          const overallScore = getOverallScore(
+            json.cleanliness,
+            json.service,
+            json.amenity
+          );
+          appdata.push({
+            'hotel': json.hotel,
+            'location': json.location,
+            'cleanliness': json.cleanliness,
+            'service': json.service,
+            'amenity': json.amenity,
+            'overallexperience': overallScore,
+          });
+          response.writeHead(200, 'OK', { 'Content-Type': 'text/plain' });
+          response.end(JSON.stringify(appdata));
+        }
+        break;
+      case '/delete':
+        {
+          const json = JSON.parse(dataString);
+          const overallScore = getOverallScore(
+            json.cleanliness,
+            json.service,
+            json.amenity
+          );
+          json['overallexperience'] = overallScore
+          let index = -1;
+          for(let i = 0; i < appdata.length; i++) {
+            if(JSON.stringify(appdata[i]) === JSON.stringify(json)) {index = i}
+          }
+          appdata.splice(index, 1);
+          response.writeHead(200, 'OK', { 'Content-Type': 'text/plain' });
+          response.end(JSON.stringify(appdata));
+        }
+        break;
+
+      case '/edit':
+        {
+          const json = JSON.parse(dataString);
+          
+          const overallScoreForOriginal = getOverallScore(
+            json[0].cleanliness,
+            json[0].service,
+            json[0].amenity
+          );
+          const overallScore = getOverallScore(
+            json[1].cleanliness,
+            json[1].service,
+            json[1].amenity
+          );
+          json[0]['overallexperience'] = overallScoreForOriginal
+          let index = -1;
+          for(let i = 0; i < appdata.length; i++) {
+            if(JSON.stringify(appdata[i]) === JSON.stringify(json[0])) {index = i}
+          }
+          appdata.splice(index, 1, 
+          {
+          'hotel': json[1].hotel,
+          'location': json[1].location,
+          'cleanliness': json[1].cleanliness,
+          'service': json[1].service,
+          'amenity': json[1].amenity,
+          'overallexperience': overallScore
+          });
+          response.writeHead(200, 'OK', { 'Content-Type': 'text/plain' });
+          response.end(JSON.stringify(appdata));
+        }
+        break
     }
-  })
+  });
+};
 
-}
+const sendFile = function (response, filename) {
+  const type = mime.getType(filename);
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+  fs.readFile(filename, function (err, content) {
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { 'Content-Type': type });
+      response.end(content);
+    } else {
+      // file not found, error code 404
+      response.writeHeader(404);
+      response.end('404 Error: File Not Found');
+    }
+  });
+};
 
-   fs.readFile( filename, function( err, content ) {
-
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
-      
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
-
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
-}
-
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port);
