2a8cbf055825ca1ca389d2d50d36c581b2f071b8
justgaret23 <sheehan.garrett23@gmail.com>
2021-09-05 20:18:13 -0400

    Lots of changes actually

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,39 +7,6 @@ console.log("Welcome to assignment 2!")
 //have edit function return data as well
 
 
-function addData(appdata){
-    let addedName = document.getElementById("player-name");
-    let addedScore = document.getElementById("player-score");
-    //let table = document.getElementById("game-leaderboard");
-
-    let addedRank;
-    let currentScore;
-    let isRankPlaced;
-    for(let i = 1; i < appdata.length; i++){
-        if(!isRankPlaced){
-            currentScore = appdata[i].rank;
-
-            //Check to see if the new score exceeds the analyzed score
-            if(addedScore.innerHTML > currentScore){
-                //If it does
-                // -give the current rank to the added rank 
-                // -lower the added rank
-                addedRank = i;
-                appdata[i].rank += 1;
-            }
-        } else {
-            //Lower the rank of the other appdata 
-            appdata[i].rank += 1;
-        }
-        
-    }
-
-    //Push the new data to appdata and then sort the appdata
-    appdata.push([addedRank, addedName, addedScore]);
-    appdata.sort((a, b) => {
-        return a.rank - b.rank;
-    });
-}
 
 function deleteData(appdata){
     
@@ -68,6 +35,174 @@ function addRow(){
 
     
 }
+
+function makeTableFromData(appdata){
+    let table = document.getElementById("game-leaderboard");
+    let data = Object.keys(appdata[0]);
+
+    table.innerHTML = '';
+    generateTableHead(table, data);
+    generateTable(table, appdata);
+
+}
+
+function generateTableHead(table, data){
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+
+    for (let key of data) {
+        let th = document.createElement("th");
+        let text = document.createTextNode(key);
+        th.appendChild(text);
+        row.appendChild(th);
+      }
+
+}
+
+function generateTable(table, data){
+
+    let i = 0;
+    
+    for(let element of data){
+        i++;
+        let row = table.insertRow();
+        for(key in element){
+            let cell = row.insertCell();
+            let text = document.createTextNode(element[key]);
+            cell.appendChild(text);
+        }
+        let deleteButton = document.createElement("button");
+        deleteButton.id = i;
+        row.appendChild(deleteButton);
+    }
+    
+}
+
+
+////////////// 
+//Game Code
+//////////////
+
+let radius = 100;
+let x, y;
+let score = 0;
+let r, g, b;
+let level = 1;
+let isCircleClicked = true;
+let gameOver = false;
+let timeInterval = 1500;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  x = random(windowWidth);
+  y = random(windowHeight);
+  r = random(255);
+  g = random(255);
+  b = random(255);
+}
+
+function draw() {
+    console.log(gameOver)
+  background(0);
+  if(!gameOver){
+    fill(r, g, b)
+    noStroke();
+    ellipse(x, y, radius*3, radius*3);
+    text("Score: " + score, 10, 20);
+    text("Level: " + level, 10, 60);
+    textSize(30);
+  } else {
+    text("Game Over!", windowWidth/2, windowHeight/2);
+    textSize(60);
+  }
+}
+  
+function mousePressed(){
+  let d = dist(mouseX, mouseY, x, y);
+    if (d < radius) {
+      newCircle();
+      score++;
+
+      
+      if(score % 10 === 0 && score < 100){
+        radius /= 1.1;
+        level++;
+        timeInterval -= 50;
+      }
+
+      /*
+      if (score == 10) {
+        radius /= 1.3;
+        level++;
+      }
+      
+      if (score == 20) {
+        radius /= 1.3;
+        level++;
+      }
+      
+      if (score == 30) {
+        radius /= 1.3;
+        level++;
+      }
+      
+      if (score == 40) {
+        radius /= 1.3;
+        level++;
+      }
+      
+      if (score == 50) {
+        radius/= 1.3;
+        level++;
+      }
+      
+      if(score == 60) {
+        radius /= 1.3;
+        level++;
+      }
+      
+      if(score == 70) {
+        radius /= 1.3;
+        level++;
+      }
+      
+      if (score == 80) {
+        radius /= 1.3;
+        level++;
+      }
+      
+      if(score == 90) {
+        radius /= 1.3;
+        level++;
+      }
+      
+      if(score == 100) {
+        radius /= 1.3;
+        level++;
+      }
+      */
+  
+     isCircleClicked = true;
+  
+    }
+      
+    }
+
+function newCircle(){
+    if(isCircleClicked){
+        x = random(windowWidth);
+        y = random(windowHeight);
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        isCircleClicked = false;
+    } else {
+        
+        //gameOver = true;
+    }
+}
+
+setInterval(newCircle, timeInterval);
 
 /*
 //Sorts the HTML table, allowing for a accurate ranked leaderboard
--- server.improved.js
+++ server.improved.js
@@ -7,9 +7,7 @@ const http = require( 'http' ),
       port = 3000
 
 const appdata = [
-  { 'name': 'oops', 'score': 1999, 'rank': 2},
-  { 'name': 'honda', 'score': 2004, 'rank': 1 },
-  { 'name': 'ford', 'score': 1987, 'rank': 3} 
+  {'name': 'm', 'score': 1999, 'rank': 1},
 ]
 
 //get/post request that lets me get app data
@@ -19,7 +17,7 @@ const server = http.createServer( function( request,response ) {
     handleGet( request, response )    
   }else if( request.method === 'POST' ){
     if(request.url === "/submit-player-score"){
-      addData(appdata);
+
     } else if(request.url === "/delete-player-score"){
       
     }
@@ -32,14 +30,7 @@ const server = http.createServer( function( request,response ) {
 const handleGet = function( request, response ) {
   const filename = dir + request.url.slice( 1 ) 
 
-  //Sort the array depending on player rank
-  appdata.sort((a,b) => a.rank - b.rank);
 
-  if(appdata.isArray(json)){
-    json
-      .map()
-      .forEach()
-  }
 
   if( request.url === '/' ) {
     sendFile( response, 'public/index.html' )
@@ -50,23 +41,48 @@ const handleGet = function( request, response ) {
 
 const handlePost = function( request, response ) {
   let dataString = ''
+  console.log(request.url)
 
   request.on( 'data', function( data ) {
       dataString += data 
   })
 
   request.on( 'end', function() {
+    console.log("data parsed successfully")
     console.log( JSON.parse( dataString ) )
+    
+
+    console.log("Before: ");
+    for(let i = 0; i < appdata.length; i++){
+      console.log(appdata[i]);
+    }
+
+    if(request.url === "/submit"){
+      let newPlayerData = JSON.parse( dataString);
+      addData(appdata, newPlayerData.playername, newPlayerData.playerscore);
+    } else if(request.url === "/deleteScore"){
+      console.log("poggies")
+      let deleteRequest = JSON.parse(dataString);
+      removeData(appdata, deleteRequest.playername);
+    }
+    
+
+    console.log("After: " + appdata)
+    for(let i = 0; i < appdata.length; i++){
+      console.log(appdata[i]);
+    }
 
     // ... do something with the data here!!!
 
-    
-    
+
 
     response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
     //if we submit a request that requires us to get the data
       //response.end(appdata)
-    response.end(appdata)
+   
+
+    console.log(JSON.stringify(appdata))
+    response.end(JSON.stringify(appdata))
   })
 }
 
@@ -93,5 +109,74 @@ const sendFile = function( response, filename ) {
 }
 
 server.listen( process.env.PORT || port )
+
+
+function addData(appdata, playerName, playerScore){
+
+  console.log("New player score: " + playerScore);
+  let addedName = playerName;
+  let addedScore = parseInt(playerScore);
+
+  let addedRank;
+  let currentScore;
+  let isRankPlaced;
+  for(let i = 0; i < appdata.length; i++){
+      if(!isRankPlaced){
+        currentScore = appdata[i].score;
+        console.log("Examining " + appdata[i].name + " in rank " + appdata[i].rank)
+
+        //Check to see if the new score exceeds the analyzed score
+        if(addedScore > currentScore){
+            console.log(addedScore + " is higher than " + currentScore + " in rank" + appdata[i].rank)
+            //If it does
+            // -give the current rank to the added rank 
+            // -lower the added rank
+            addedRank = i+1;
+            appdata[i].rank += 1;
+            isRankPlaced = true;
+        }
+      } else {
+          //Lower the rank of the other appdata 
+          appdata[i].rank += 1;
+      }
+      
+  }
+
+  //If this is the lowest score, indicate it's the last rank
+  if(!isRankPlaced){
+    addedRank = appdata.length + 1;
+  }
+
+
+  //Push the new data to appdata and then sort the appdata
+  appdata.push({name: addedName, score: addedScore, rank: addedRank});
+  appdata.sort((a, b) => {
+      return a.rank - b.rank;
+  });
+}
+
+//Removes a piece of data from the table
+function removeData(appdata, row){
+  console.log("CURRENT ROW: " + row)
+
+  //remove data
+  let isItemRemoved = false;
+  for(let element of appdata){
+    if(element.name === row){
+      appdata.splice(element.rank-1, 1);
+      isItemRemoved = true;
+      break;
+    }
+  }
+  if(!isItemRemoved){
+    console.log("Something fishy is happening...");
+  } else {
+    //Reassign item ranks
+    for(let i = 0; i < appdata.length; i++){
+      appdata[i].rank = i+1;
+    }
+  }
+}
+
 
 
