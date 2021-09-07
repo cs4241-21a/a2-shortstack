0e4d808e64bafd1e70237e152cd4ff0383fb2c0c
trbouwens <42808108+trbouwens@users.noreply.github.com>
2021-09-07 15:23:28 -0400

    Functionality and Inital setup

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,101 @@
 // Add some Javascript code here, to run on the front end.
 
-console.log("Welcome to assignment 2!")
+function addRoll() {
+    const input = document.getElementById("add"),
+          json = {
+              character: input.elements[0].value,
+              diceType: input.elements[1].value,
+              quantity: input.elements[2].value,
+              modifier: input.elements[3].value,
+          },
+          body = JSON.stringify(json);
+
+          fetch( '/add', {
+              method:'POST',
+              body
+          }).then(function( response ) {
+              if(response.status === 200){
+                  console.log(body);
+                  update(response)
+                  return true;
+              }
+          });
+
+          return false;
+}
+
+function deleteRoll() {
+    const input = document.getElementById("delete"),
+    json = {
+      id: input.elements[0].value
+    }
+    body = JSON.stringify(json);
+
+    fetch( '/delete', {
+        method:'POST',
+        body
+    }).then(function( response ) {
+
+        if(response.status === 200){
+            console.log(body);
+            update(response)
+            return true;
+        }
+    });
+
+    return false;
+}
+
+function clearRolls() {
+    fetch( '/clear', {
+      method:'GET',
+    }).then(function( response ) {
+
+      if(response.status === 200){
+          update(response);
+          return true;
+      }
+
+    });
+    return false;
+}
+
+function updateOnLoad() {
+  fetch( '/load', {
+    method:'GET',
+  }).then(function( response ) {
+
+    if(response.status === 200){
+        update(response);
+        return true;
+    }
+
+  });
+  return false;
+}
+
+function update(results) {
+    let table = document.getElementById("table_list");
+    let newTable = document.createElement("tbody");
+
+    table.replaceChild(newTable, table.lastChild);
+
+    console.log(results)
+
+    results.json().then(function(data) {
+        let nRows = data.nRows;
+        let rowData = data.rowData;
+
+        for (let i = 0; i < nRows; i++) {
+            let newRow = newTable.insertRow(i);
+            newRow.insertCell(0).innerHTML = `${rowData[i].id}`;
+            newRow.insertCell(1).innerHTML = `${rowData[i].character}`;
+            newRow.insertCell(2).innerHTML = `d${rowData[i].diceType}`;
+            newRow.insertCell(3).innerHTML = `${rowData[i].quantity}`;
+            newRow.insertCell(4).innerHTML = `${rowData[i].modifier}`;
+            newRow.insertCell(5).innerHTML = `${rowData[i].roll}`;
+        }
+      })
+
+  return 0
+}
--- server.improved.js
+++ server.improved.js
@@ -6,49 +6,168 @@ const http = require( 'http' ),
       dir  = 'public/',
       port = 3000
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+let appdata = []
+
+let dataCount = 0
+let id = 0
+
+const rollDice = function( type, quantity, mod) {
+  totalRoll = 0
+
+  for (i = 0; i < parseInt(quantity,10); i++) {
+    let baseRoll = Math.floor(Math.random() * parseInt(type,10));
+    totalRoll += (baseRoll + 1)
+  }
+
+  return(totalRoll + parseInt(mod,10))
+}
+
 
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
-    handleGet( request, response )    
+    handleGet( request, response )
   }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+    handlePost( request, response )
   }
 })
 
+//
 const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+  const filename = dir + request.url.slice( 1 )
 
   if( request.url === '/' ) {
     sendFile( response, 'public/index.html' )
-  }else{
+  }
+  else if ( request.url === "/clear") {
+    clearRolls(response)
+  }
+  else if (request.url === "/load") {
+    sendTable(response)
+  }
+  else{
     sendFile( response, filename )
   }
 }
 
+//
 const handlePost = function( request, response ) {
   let dataString = ''
 
   request.on( 'data', function( data ) {
-      dataString += data 
+      dataString += data
   })
 
   request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+    let data = JSON.parse(dataString)
 
-    // ... do something with the data here!!!
+    if(request.url === "/add") {
+      if(addRoll(data)) {
+        sendTable(response)
+      }
+      else{
+        response.writeHead(400, "Add failed", {'Content-Type': 'text/plain'});
+        response.end();
+      }
+    }
+    else if(request.url === "/delete") {
+      if(deleteRoll(data)) {
+        sendTable(response)
+      }
+      else{
+        response.writeHead(400, "Delete failed", {'Content-Type': 'text/plain'});
+        response.end();
+      }
+    }
 
     response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
     response.end()
   })
 }
 
+
+//
+const addRoll = function(data) {
+  /*
+  Data Validation:
+    character must not be: ""
+    quantity must not be: NaN, <= 0
+    modifier must not be: NaN, < 0
+  */
+  if (data.quantity <= 0 || data.modifier < 0 ||
+      isNaN(data.quantity) || isNaN(data.modifier) ||
+      data.character.length == 0) {
+
+      return false
+  }
+
+  thisRoll = rollDice(data.diceType,data.quantity,data.modifier)
+
+  json = {
+      "id" : id,
+      "character" : data.character,
+      "diceType" : data.diceType,
+      "quantity" : data.quantity,
+      "modifier" : data.modifier,
+      "roll" : thisRoll
+  }
+
+  appdata.push(json)
+
+  id++
+  dataCount++
+
+  console.log("TO ADD:")
+  console.log(json)
+
+  return true
+}
+
+//
+const deleteRoll = function(data) {
+  /*
+  Data Validation:
+    Only one of Character or ID must not be none.
+    ID must not be : NaN, < 0, > id
+    Charater must not be: "" if ID is none.
+  */
+  if (data.id < 0 || data.id > id || isNaN(data.id)) {
+    return false
+  }
+
+  // Loop Through the data to find correct row.
+  let index = -1
+  for(let i = 0; i < dataCount; i++) {
+      if (appdata[i].id == data.id) {
+        index = i
+        break
+      }
+  }
+
+  if(index === -1) {
+    return false
+  }
+
+  appdata.splice(index, 1)
+  dataCount--
+
+  console.log("TO DELETE:")
+  console.log(appdata[index])
+
+  return true
+
+}
+
+//
+const clearRolls = function(response) {
+  appdata = []
+  dataCount = 0
+  id = 0
+  sendTable(response)
+}
+
+//
 const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+   const type = mime.getType( filename )
 
    fs.readFile( filename, function( err, content ) {
 
@@ -67,6 +186,22 @@ const sendFile = function( response, filename ) {
 
      }
    })
+}
+
+//
+const sendTable = function(response) {
+    let json = {
+        "nRows": dataCount,
+        "rowData": []
+    }
+
+    for(let i = 0; i < dataCount; i++){
+        json["rowData"].push(appdata[i]);
+    }
+
+    let body = JSON.stringify(json);
+    response.writeHead(200, "OK", {"Content-Type": "text/plain"});
+    response.end(body);
 }
 
 server.listen( process.env.PORT || port )
