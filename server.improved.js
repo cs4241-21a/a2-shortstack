fac362e7277f1fe3809c3cdb4dffa65328e156ca
BlueOcean090 <kashara>
2021-09-07 13:50:10 -0400

    TA/SA commit

--- server.improved.js
+++ server.improved.js
@@ -1,23 +1,23 @@
 const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+    fs   = require( 'fs' ),
+    // IMPORTANT: you must run `npm install` in the directory for this assignment
+    // to install the mime library used in the following line of code
+    mime = require( 'mime' ),
+    dir  = 'public/',
+    port = 3000
 
 let appdata = []
 
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
-    handleGet( request, response )    
+    handleGet( request, response )
   }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+    handlePost( request, response )
   }
 })
 
 const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+  const filename = dir + request.url.slice( 1 )
 
   if( request.url === '/' ) {
     sendFile( response, 'public/index.html' )
@@ -30,40 +30,48 @@ const handlePost = function( request, response ) {
   let dataString = ''
 
   request.on( 'data', function( data ) {
-      dataString += data 
+    dataString += data
   })
 
   request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
-    appdata.push(dataString)
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    for (let length=0; length<appdata.length; length++){
-      console.log(appdata[length])
+    switch(request.url){
+      case '/adddata':
+      {
+        appdata.push(dataString)
+        response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+        response.end(JSON.stringfy(appdata));
+      } break;
+        /*
+        add more cases here if you want to edit or delete and handle them accordingly
+        */
     }
-    response.end()
   })
 }
 
+const processReceivedData = function (){
+  /* Write some code here to process the data you have received from the client */
+}
+
 const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+  const type = mime.getType( filename )
 
-   fs.readFile( filename, function( err, content ) {
+  fs.readFile( filename, function( err, content ) {
 
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
+    // if the error = null, then we've loaded the file successfully
+    if( err === null ) {
 
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
+      // status code: https://httpstatuses.com
+      response.writeHeader( 200, { 'Content-Type': type })
+      response.end( content )
 
-     }else{
+    }else{
 
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
+      // file not found, error code 404
+      response.writeHeader( 404 )
+      response.end( '404 Error: File Not Found' )
 
-     }
-   })
+    }
+  })
 }
 
 server.listen( process.env.PORT || port )
