90a26f1d96d061a471deb5ee34ce498eb0b9265d
Timothy Goon <t.kgoonx@gmail.com>
2021-09-06 20:35:18 -0400

    Back end and functionality of app completed.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,163 @@
 // Add some Javascript code here, to run on the front end.
 
 console.log("Welcome to assignment 2!")
+
+// Update the tasks table data
+let updateTasks = (tasks) => {
+
+    const holder = document.querySelector('#task-holder');
+    holder.innerHTML = '';
+
+    tasks.forEach(element => {
+
+        // Create table row
+        const row = document.createElement('tr');
+
+        const titleData = document.createElement('td');
+        const titleInput = document.createElement('input');
+        titleData.appendChild(titleInput);
+        titleInput.value = element.title;
+        titleInput.id = `title-${element.title}`;
+
+        const descriptionData = document.createElement('td');
+        const descriptionInput = document.createElement('input');
+        descriptionData.appendChild(descriptionInput);
+        descriptionInput.value = element.description;
+        descriptionInput.id = `description-${element.title}`;
+
+        const priorityData = document.createElement('td');
+        const priorityInput = document.createElement('input');
+        priorityInput.type = 'number';
+        priorityInput.min = 0;
+        priorityInput.max = 10;
+        priorityData.appendChild(priorityInput);
+        priorityInput.value = element.priority;
+        priorityInput.id = `priority-${element.title}`;
+
+        const deadlineData = document.createElement('td');
+        const deadlineInput = document.createElement('input');
+        deadlineInput.readOnly = true;
+        deadlineData.appendChild(deadlineInput);
+        deadlineInput.value = element.deadline;
+
+        const delBtn = document.createElement('button');
+        delBtn.appendChild(document.createTextNode('Delete'));
+        delBtn.onclick = () => {
+
+            fetch('/delete', {
+                method: 'POST',
+                body: JSON.stringify({title: element.title})
+            }).then(async (data) => {
+                const tasks = await data.json();
+
+                updateTasks(tasks);
+            }).catch((err) => {
+                console.log(err);
+            });
+        };
+
+        const editBtn = document.createElement('button');
+        editBtn.appendChild(document.createTextNode('Edit'));
+        editBtn.onclick = (e) => {
+
+            const priority = document.querySelector(`#priority-${element.title}`);
+
+            if ((!priority.valueAsNumber && priority.valueAsNumber !== 0) || priority.valueAsNumber < 0 || priority.valueAsNumber > 10) {
+                alert('Priority must be 0 - 10');
+                return;
+            }
+
+            fetch('/edit', {
+                method: 'POST',
+                body: JSON.stringify({
+                    oldTitle: element.title,
+                    newTitle: document.querySelector(`#title-${element.title}`).value,
+                    description: document.querySelector(`#description-${element.title}`).value,
+                    priority: priority.valueAsNumber
+                })
+            }).then(async (data) => {
+                const tasks = await data.json();
+
+                if (tasks.error) {
+                    alert(tasks.error);
+                    return;
+                }
+
+                updateTasks(tasks);
+            }).catch((err) => {
+                console.log(err);
+            });
+        };
+
+        // Append everything together
+        row.appendChild(titleData);
+        row.appendChild(descriptionData);
+        row.appendChild(priorityData);
+        row.appendChild(deadlineData);
+
+        row.appendChild(editBtn);
+        row.appendChild(delBtn);
+
+        holder.appendChild(row);
+    });
+}
+
+const submit = function (e) {
+    // prevent default form action from being carried out
+    e.preventDefault();
+
+    // Get and format today's date
+    var today = new Date();
+    const dateString = `${String(today.getMonth() + 1).padStart(2, '0')}/` +
+        `${String(today.getDate()).padStart(2, '0')}/${today.getFullYear()}`;
+
+    const title = document.querySelector('#title');
+    const description = document.querySelector('#description');
+
+    const priority = document.querySelector('#priority');
+    if ((!priority.valueAsNumber && priority.valueAsNumber !== 0) || priority.valueAsNumber < 0 || priority.valueAsNumber > 10) {
+        alert('Priority must be 0 - 10');
+        return;
+    }
+
+    const json = {
+        title: title.value,
+        description: description.value,
+        dateCreated: dateString,
+        priority: priority.valueAsNumber
+    };
+    const body = JSON.stringify(json)
+
+    fetch('/submit', {
+        method: 'POST',
+        body
+    })
+        .then(async function (response) {
+            const tasks = await response.json();
+
+            if (tasks.error) {
+                alert(tasks.error);
+                return;
+            }
+
+            updateTasks(tasks);
+        });
+
+    return false;
+}
+
+window.onload = function () {
+    const button = document.querySelector('button')
+    button.onclick = submit;
+
+    // Get initial table of data from server
+    fetch('/get-data', {
+        method: 'GET'
+    }).then(async (data) => {
+        const tasks = await data.json();
+
+        updateTasks(tasks);
+    }).catch((err) => {
+        console.log(err);
+    });
+}
--- server.improved.js
+++ server.improved.js
@@ -1,72 +1,170 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require('http'),
+  fs = require('fs'),
+  // IMPORTANT: you must run `npm install` in the directory for this assignment
+  // to install the mime library used in the following line of code
+  mime = require('mime'),
+  dir = 'public/',
+  port = 3000
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+let appdata = [];
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+const server = http.createServer(function (request, response) {
+  if (request.method === 'GET') {
+    handleGet(request, response)
+  } else if (request.method === 'POST') {
+    handlePost(request, response)
   }
 })
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+const handleGet = function (request, response) {
+  const filename = dir + request.url.slice(1)
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
+  if (request.url === '/') {
+    sendFile(response, 'public/index.html')
+  } else if (request.url === '/get-data') {
+    response.writeHead(200, "OK", { 'Content-Type': 'application/json' });
+    response.end(JSON.stringify(appdata));
+  } else {
+    sendFile(response, filename)
   }
 }
 
-const handlePost = function( request, response ) {
+const handlePost = function (request, response) {
   let dataString = ''
 
-  request.on( 'data', function( data ) {
-      dataString += data 
+  request.on('data', function (data) {
+    dataString += data
   })
 
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+  request.on('end', function () {
+    let json = JSON.parse(dataString);
 
-    // ... do something with the data here!!!
+    if(request.url === '/submit'){
+      let dupe = false;
+      appdata.forEach((element) => {
+        if (element.title === json.title) {
+          response.writeHead(200, "OK", { 'Content-Type': 'application/json' });
+          const resData = { ...appdata, error: 'Duplicate Task titles not allowed'};
+          response.end(JSON.stringify(resData));
+          dupe = true;
+        }
+      });
+  
+      if (dupe)
+        return;
+  
+      appdata.push(
+        {
+          title: json.title,
+          description: json.description,
+          priority: json.priority,
+          dateCreated: json.dateCreated,
+          deadline: createDeadline(json.dateCreated, json.priority)
+        }
+      );
+  
+      response.writeHead(200, "OK", { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify(appdata));
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
+    } else if (request.url === '/delete') {
+
+      // Delete task
+      appdata = appdata.filter((element) => {
+        return json.title !== element.title;
+      });
+
+      response.writeHead(200, "OK", { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify(appdata));
+    } else if (request.url === '/edit') {
+      // Edit task
+      appdata = appdata.map((element) => {
+        if(json.oldTitle === element.title) {
+          element.title = json.newTitle;
+          element.description = json.description;
+          element.priority = json.priority;
+          element.deadline = createDeadline(element.dateCreated, json.priority)
+        }
+
+        return element;
+      });
+
+      response.writeHead(200, "OK", { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify(appdata));
+    }
+    
+  });
+}
+
+const sendFile = function (response, filename) {
+  const type = mime.getType(filename)
+
+  fs.readFile(filename, function (err, content) {
+
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
+
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { 'Content-Type': type })
+      response.end(content)
+
+    } else {
+
+      // file not found, error code 404
+      response.writeHeader(404)
+      response.end('404 Error: File Not Found')
+
+    }
   })
 }
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+let createDeadline = (dateCreated, priority) => {
 
-   fs.readFile( filename, function( err, content ) {
+  let month = parseInt(dateCreated.substr(0, 2));
+  let day = parseInt(dateCreated.substr(3, 2));
+  let year = parseInt(dateCreated.substr(6, 4));
+  let deadLine = new Date();
+  deadLine.setDate(day);
+  deadLine.setMonth(month-1);
+  deadLine.setFullYear(year);
 
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
+  switch (priority) {
+    case 0:
+      deadLine.setFullYear(deadLine.getFullYear() + 1);
+      break;
+    case 1:
+      deadLine.setMonth(deadLine.getMonth() + 6);
+      break;
+    case 2:
+      deadLine.setMonth(deadLine.getMonth() + 3);
+      break;
+    case 3:
+      deadLine.setMonth(deadLine.getMonth() + 2);
+      break;
+    case 4:
+      deadLine.setMonth(deadLine.getMonth() + 1);
+      break;
+    case 5:
+      deadLine.setDate(deadLine.getDate() + 15);
+      break;
+    case 6:
+      deadLine.setDate(deadLine.getDate() + 7);
+      break;
+    case 7:
+      deadLine.setDate(deadLine.getDate() + 3);
+      break;
+    case 8:
+      deadLine.setDate(deadLine.getDate() + 2);
+      break;
+    case 9:
+      deadLine.setDate(deadLine.getDate() + 1);
+      break;
+    case 10:
+      // deadline is today no change
+      break;
+  }
 
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
-
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
+  return `${String(deadLine.getMonth() + 1).padStart(2, '0')}/` +
+    `${String(deadLine.getDate()).padStart(2, '0')}/${deadLine.getFullYear()}`;
 }
 
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port)
