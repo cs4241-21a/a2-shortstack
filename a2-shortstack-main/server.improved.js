f7a886cd74efc72a70e62811c22c08a7ecec2544
palomagonzalez <57199825+palomagonzalez@users.noreply.github.com>
2021-09-08 19:28:38 -0400

    Add files via upload

---
+++ a2-shortstack-main/server.improved.js
@@ -0,0 +1,92 @@
+const http = require( 'http' ),
+      fs   = require( 'fs' ),
+      // IMPORTANT: you must run `npm install` in the directory for this assignment
+      // to install the mime library used in the following line of code
+      mime = require( 'mime' ),
+      dir  = 'public/',
+      port = 3000
+
+let appdata = [
+   
+]
+
+const server = http.createServer( function( request,response ) {
+  if( request.method === 'GET' ) {
+    handleGet( request, response )    
+  }else if( request.method === 'POST' ){
+    handlePost( request, response ) 
+  }
+})
+
+const handleGet = function( request, response ) {
+  const filename = dir + request.url.slice( 1 ) 
+
+  if( request.url === '/' ) {
+    sendFile( response, 'public/index.html' )
+  }else{
+    sendFile( response, filename )
+  }
+}
+
+const handlePost = function( request, response ) {
+  let dataString = ''
+
+  request.on( 'data', function( data ) {
+      dataString += data 
+  })
+
+  request.on( 'end', function() {
+    const json = JSON.parse( dataString ) 
+    
+    // ... do something with the data here!!!
+
+    if(request.url == '/submit'){
+
+      appdata.push(json);
+
+    } else if (request.url == '/getdata'){
+
+      response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+      response.end( JSON.stringify(appdata))
+
+      return 
+
+    } /* else if (request.url == '/delete'){
+
+      console.log('this is app data before filtering' + appdata)
+      console.log('this json.groCeryItem' + json.groceryItem)
+      appdata = appdata.filter(j => j.groceryItem !== json.groceryItem)
+      console.log('this is app data after filtering' + appdata)
+    }*/
+
+    json.groceryTotalPrice = json.groceryQuantity * json.groceryUnitPrice
+
+    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+    response.end( JSON.stringify(json))
+    
+  })
+}
+
+const sendFile = function( response, filename ) {
+   const type = mime.getType( filename ) 
+
+   fs.readFile( filename, function( err, content ) {
+
+     // if the error = null, then we've loaded the file successfully
+     if( err === null ) {
+
+       // status code: https://httpstatuses.com
+       response.writeHeader( 200, { 'Content-Type': type })
+       response.end( content )
+
+     }else{
+
+       // file not found, error code 404
+       response.writeHeader( 404 )
+       response.end( '404 Error: File Not Found' )
+
+     }
+   })
+}
+
+server.listen( process.env.PORT || port )
