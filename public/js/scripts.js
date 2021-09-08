60d460fa8615454b2bd28f7858e88e1241aa8c47
oblaufuss <ocblaufuss@wpi.edu>
2021-09-07 23:04:41 -0400

    populate all 3 values + sort by date

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,58 +1,99 @@
+'use strict';
 
-  const submit = function( e ) {
+const horgTypes = ["Archignathidae","Haplognathidae","Acutignathidae", "Tridentidae",
+ "Toxodentidae", "Mycognathidae", "Corrugatidae", "Other", "Undescribed"];
+
+const submit = function (e) {
     // prevent default form action from being carried out
     e.preventDefault()
 
-    const input = document.querySelector( '#yourname' ),
-          cars = document.querySelector('#cars'),
-          json = { yourname: input.value, cars: cars.value },
-          body = JSON.stringify( json )
+    const date = document.querySelector('#date'),
+        family = document.querySelector('#family'),
+        notes = document.querySelector('#notes'),
+        json = { date: date.value, family: family.value, notes:notes.value },
+        body = JSON.stringify(json)
 
-    fetch( '/submit', {
-      method:'POST',
-      body 
+    fetch('/submit', {
+        method: 'POST',
+        body
     })
-    .then( function( response ) {
-      console.log(response)
-      return response.json()
-    })
-    .then( function( json ) {
-      console.log(json)
+        .then(function (response) {
+            console.log(response)
+            return response.json()
+        })
+        .then(function (json) {
+            console.log(json)
 
-      //get table to add them to
-      let horgTable = document.getElementById("entries-wrapper")
-      
-      /*for ( let i = 0; i < json.length; i++){
-        const element = document.createElement("p")
-        element.innerText = ''+ json[i].yourname + json[i].cars;
-      document.body.appendChild(element)
-      }
-      */
+            //clear the table
+            let horgTable = document.getElementById("entries-wrapper");
+            horgTable.innerHTML = "";
 
-      //create HORG entry
-      let horgEntry = document.createElement("div");
-      horgEntry.classList.add("entry");
+            //sort entries by date
+            json = json.sort((a,b)=> (a.date < b.date) ? 1 : -1);
 
-      //add data
-      let dateSpan = document.createElement("span");
-      dateSpan.innerHTML="testtext";
-      horgEntry.appendChild(dateSpan);
+            //fill the table
+            for ( let i = 0; i < json.length; i++){
+                let horgEntry = populateHorgRow(json[i].date, json[i].family, json[i].notes);
+                horgTable.appendChild(horgEntry);
+            }
 
-      //add x-button
-      let btnX = document.createElement("button");
-      btnX.innerHTML="x";
-      btnX.classList.add("remove");
-      horgEntry.appendChild(btnX);
-
-      //add to list!
-      horgTable.appendChild(horgEntry);
-     
-    })
+        })
 
     return false
-  }
+}
 
-  window.onload = function() {
-    const button = document.querySelector( 'button' )
+const deleteEntry = function () {
+    //todo
+}
+
+const populateHorgRow = function (date, type, notes) {
+    //create HORG entry
+    let horgEntry = document.createElement("div");
+    horgEntry.classList.add("entry");
+
+    //add data-
+    //date
+    let dateSpan = document.createElement("span");
+    dateSpan.innerHTML = date;
+    horgEntry.appendChild(dateSpan);
+    //type
+    let typeSpan = document.createElement("span");
+    typeSpan.innerHTML = type;
+    horgEntry.appendChild(typeSpan);
+    //notes
+    let notesSpan = document.createElement("details");
+    let notesLabel = document.createElement("summary");
+    notesLabel.innerHTML = "See notes..."
+    notesSpan.appendChild(notesLabel);
+    notesSpan.innerHTML += notes;
+    horgEntry.appendChild(notesSpan);
+
+    //add x-button
+    let btnX = document.createElement("button");
+    btnX.innerHTML = "x";
+    btnX.classList.add("remove");
+    btnX.onclick = deleteEntry;
+    horgEntry.appendChild(btnX);
+
+    return horgEntry;
+}
+
+window.onload = function () {
+    //remove warning
+    let jsWarn = document.getElementById("warn");
+    jsWarn.remove();
+
+    //populate taxonomic family
+    let familySelect = document.getElementById('family');
+    for (let familyName of horgTypes){
+        //console.log(familyName);
+        var opt = document.createElement('option');
+        opt.value = familyName;
+        opt.innerHTML = familyName;
+        familySelect.appendChild(opt);
+    }
+
+    //enable submit button
+    const button = document.querySelector('button')
     button.onclick = submit
-  }
+}
