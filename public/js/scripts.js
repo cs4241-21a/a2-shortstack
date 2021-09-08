dd43fa6b9424e78b6ec3df4cb43711e154649fda
Evdude33 <62618387+emllewellyn@users.noreply.github.com>
2021-09-08 17:02:52 -0400

    Final Changes

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -26,51 +26,86 @@ const submit = function (e) {
     }
 
     let d = new Date()
-    let d1 = d.getFullYear() + 1
+    let d1 = d.getFullYear()
 
     if (yearNum > d1) {
         alert("This year's models aren't released yet.")
         return false
     }
 
+    year.value = yearNum
+
     if (plateNum.value.length < 5 || plateNum.value.length > 8) {
         alert("A plate number has a min of 5 and max of 8 characters.")
         return false
     }
 
-    year.value = yearNum
     plateNum.value = plateNum.value.toUpperCase()
+    let table = document.getElementById("cartable")
+    for(let row of table.rows) {
+        if(row.cells[4].innerHTML === plateNum.value) {
+            alert("Cannot have multiple cars with the same plate number.")
+            return false
+        }
+    }
 
-    json = { yourname: name.value, make: make.value, model: model.value, year: year.value, plateNum: plateNum.value },
+    json = {
+        yourname: name.value,
+        make: make.value,
+        model: model.value,
+        year: year.value,
+        plateNum: plateNum.value
+    },
         body = JSON.stringify(json)
 
     fetch('/submit', {
         method: 'POST',
         body
     })
-    .then(response => response.text())  
-    .then(function(text) {
-        const data = JSON.parse(text)
-        const i = data.length - 1
-        let table = document.getElementById("cartable")
-        let row = table.insertRow(-1)
-        let c1 = row.insertCell(0)
-        let c2 = row.insertCell(1)
-        let c3 = row.insertCell(2)
-        let c4 = row.insertCell(3)
-        let c5 = row.insertCell(4)
-        let c6 = row.insertCell(5)
-        c1.innerHTML = data.yourname
-        c2.innerHTML = data.make
-        c3.innerHTML = data.model
-        c4.innerHTML = data.year
-        c5.innerHTML = data.plateNum
-        c6.innerHTML = data.age
-        let form = document.getElementById("carform");
-        form.reset();
-    })
+        .then(response => response.text())
+        .then(function (text) {
+            const data = JSON.parse(text)
+            const i = data.length - 1
+            let table = document.getElementById("cartable")
+            let row = table.insertRow(-1)
+            let c1 = row.insertCell(0)
+            let c2 = row.insertCell(1)
+            let c3 = row.insertCell(2)
+            let c4 = row.insertCell(3)
+            let c5 = row.insertCell(4)
+            let c6 = row.insertCell(5)
+            let c7 = row.insertCell(6)
+            c1.innerHTML = data.yourname
+            c2.innerHTML = data.make
+            c3.innerHTML = data.model
+            c4.innerHTML = data.year
+            c5.innerHTML = data.plateNum
+            c6.innerHTML = data.age
+            c7.innerHTML = '<button id="delete" onclick="remove(this)">Delete</button>'
+            let form = document.getElementById("carform");
+            form.reset();
+        })
 
     return false
+}
+
+const remove = function (obj) {
+    let form = document.getElementById("carform");
+    form.reset();
+
+    let rowNum = obj.parentNode.parentNode.rowIndex
+    let plateNumber = document.getElementById("cartable").rows[rowNum].cells[4].innerHTML
+    document.getElementById("cartable").deleteRow(rowNum)
+    const body = JSON.stringify(plateNumber)
+
+    fetch('/remove', {
+        method: 'POST',
+        body
+    })
+        .then(response => response.text())
+        .then(function (text) {
+            console.log("Deleted row from server.")
+        })
 }
 
 window.onload = function () {
--- server.improved.js
+++ server.improved.js
@@ -1,74 +1,88 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require('http'),
+  fs = require('fs'),
+  // IMPORTANT: you must run `npm install` in the directory for this assignment
+  // to install the mime library used in the following line of code
+  mime = require('mime'),
+  dir = 'public/',
+  port = 3000
 
 let appData = []
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+const server = http.createServer(function (request, response) {
+  if (request.method === 'GET') {
+    handleGet(request, response)
+  } else if (request.method === 'POST') {
+    handlePost(request, response)
   }
 })
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+const handleGet = function (request, response) {
+  const filename = dir + request.url.slice(1)
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
+  if (request.url === '/') {
+    sendFile(response, 'public/index.html')
+  } else {
+    sendFile(response, filename)
   }
 }
 
-const handlePost = function( request, response ) {
+const handlePost = function (request, response) {
+  
   let dataString = ''
-
-  request.on( 'data', function( data ) {
-      dataString += data 
+  
+  request.on('data', function (data) {
+    dataString += data
   })
 
-  request.on( 'end', function() {
-    const formData = JSON.parse( dataString )
-    let year = formData.year
-    let d = new Date()
-    let age = d.getFullYear() - year
-    formData.age = String(age);
+  if (request.url === '/submit') {
 
-    appData.push(formData)
+    request.on('end', function () {
+      const formData = JSON.parse(dataString)
+      let year = formData.year
+      let d = new Date()
+      let age = d.getFullYear() - year
+      formData.age = String(age);
 
-    console.log("json: " + JSON.stringify(formData))
+      appData.push(formData)
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end(JSON.stringify(formData))
+      console.log("json: " + JSON.stringify(formData))
+
+      response.writeHead(200, "OK", { 'Content-Type': 'text/plain' })
+      response.end(JSON.stringify(formData))
+    })
+  }
+
+  else if(request.url === '/remove') {
+    request.on('end', function() {
+      const newArr = appData.filter(car => car.plateNum != dataString.slice(1,-1))
+      appData = newArr
+      
+      response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+      response.end(dataString)
+    })
+  }
+}
+
+const sendFile = function (response, filename) {
+  const type = mime.getType(filename)
+
+  fs.readFile(filename, function (err, content) {
+
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
+
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { 'Content-Type': type })
+      response.end(content)
+
+    } else {
+
+      // file not found, error code 404
+      response.writeHeader(404)
+      response.end('404 Error: File Not Found')
+
+    }
   })
 }
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
-
-   fs.readFile( filename, function( err, content ) {
-
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
-
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
-
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
-}
-
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port)
