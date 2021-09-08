f94eebe54c1820a20a8b43d883cfdd98e1da9b6e
G3P <73323107+GP2P@users.noreply.github.com>
2021-09-08 17:23:24 +0800

    Initial Commit

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,158 @@
-// Add some Javascript code here, to run on the front end.
+let budget = 0;
+let totalIn = 0;
+let totalOut = 0;
 
-console.log("Welcome to assignment 2!")
+window.onload = function () {
+	document.getElementById('submitBudget').onclick = submitBudget
+	document.getElementById('submitEntry').onclick = submitEntry
+	document.getElementById('date').value = new Date().toLocaleDateString('en-CA')
+	refreshBudget(JSON.stringify({type: "getBudget"}))
+	refreshData()
+}
+
+const submitEntry = function (e) {
+	e.preventDefault()
+
+	const type = document.getElementById('type'),
+		date = document.getElementById('date'),
+		amount = document.getElementById('amount'),
+		category = document.getElementById('category'),
+		json = {type: type.value, date: date.value, amount: amount.value, category: category.value},
+		body = JSON.stringify(json)
+
+	amount.value = 0
+
+	fetch('/submit', {
+		method: 'POST',
+		body
+	})
+		.then(response => response.json())
+		.then(function (json) {
+			// Removes all old data
+			document.querySelectorAll(".item").forEach(each => each.remove());
+
+			// Add new data
+			console.log(json)
+			getData(json)
+		})
+
+	return false
+}
+
+const submitBudget = function (e) {
+	e.preventDefault()
+
+	const json = {type: "budget", budget: +document.getElementById('budgetNew').value},
+		body = JSON.stringify(json)
+
+	refreshBudget(body)
+
+	return false
+}
+
+const refreshBudget = function (body) {
+	fetch('/submit', {
+		method: 'POST',
+		body
+	})
+		.then(response => response.json())
+		.then(function (json) {
+			console.log(json)
+
+			if (json == null) budget = 0.00
+			else budget = json
+
+			document.getElementById('budget').innerText = "Budget: $" + json
+			document.getElementById('budgetNew').value = 0;
+
+
+			if (totalOut > budget) {
+				alert("You are over budget!")
+			}
+		})
+
+	return false
+}
+
+const refreshData = function () {
+
+	const body = JSON.stringify({type: "getData"})
+
+	fetch('/submit', {
+		method: 'POST',
+		body
+	})
+		.then(response => response.json())
+		.then(function (json) {
+			console.log(json)
+			getData(json)
+		})
+
+	return false
+}
+
+function getData(json) {
+	json.forEach(eachDay => {
+		const cardHolder = document.createElement('div')
+		cardHolder.className += "cardHolder item"
+
+		const card = document.createElement('div')
+		card.className += "card"
+
+		const dayHolder = document.createElement('div')
+		if (eachDay.I === eachDay.O) {
+			dayHolder.className += "dayHolder"
+		} else if (eachDay.I > eachDay.O) {
+			dayHolder.className += "dayHolder win"
+		} else dayHolder.className += "dayHolder lose"
+
+		const day = document.createElement('p')
+		day.className += "day"
+
+		const b = document.createElement('b')
+		b.innerText = eachDay.date
+
+		const IO = document.createElement('p')
+		IO.innerText = "In: $" + (+eachDay.I).toFixed(2) + " Out: $" + (+eachDay.O).toFixed(2)
+
+		dayHolder.appendChild(document.createElement('br'))
+		day.appendChild(b)
+		dayHolder.appendChild(day)
+		dayHolder.appendChild(IO)
+		dayHolder.appendChild(document.createElement('br'))
+		card.appendChild(dayHolder)
+
+		eachDay.transactions.forEach(eachTransaction => {
+
+			const detailHolder = document.createElement('div')
+			detailHolder.className += "detailHolder"
+
+			const catagory = document.createElement('p')
+			catagory.innerText = eachTransaction.category
+
+			const amount = document.createElement('p')
+			if (eachTransaction.isIn) {
+				amount.innerText = "+" + eachTransaction.amount
+				totalIn += +(+eachTransaction.amount).toFixed(2)
+			} else {
+				amount.innerText = "-" + eachTransaction.amount
+				totalOut += +(+eachTransaction.amount).toFixed(2)
+			}
+
+			detailHolder.appendChild(catagory)
+			detailHolder.appendChild(amount)
+			card.appendChild(detailHolder)
+		})
+		cardHolder.appendChild(card)
+		document.body.appendChild(cardHolder)
+	})
+
+	document.getElementById('totalIn').innerText = "Total Income: $" + totalIn.toFixed(2)
+	document.getElementById('totalOut').innerText = "Total Expense: $" + totalOut.toFixed(2)
+
+	console.log(totalOut)
+	console.log(budget)
+	if (totalOut > budget) {
+		alert("You are over budget!")
+	}
+}
--- server.improved.js
+++ server.improved.js
@@ -1,72 +1,158 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require('http'),
+	fs = require('fs'),
+	mime = require('mime'),
+	dir = 'public/',
+	port = 3000
+
+let budget = 2500;
 
 const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
+	{
+		"date": "2021-09-08",
+		"I": 4273.82,
+		"O": 47.32,
+		"transactions": [
+			{"isIn": false, "date": "2021-09-08", "amount": "34.99", "category": "Brunch"},
+			{"isIn": false, "date": "2021-09-08", "amount": "12.33", "category": "Uber"},
+			{"isIn": true, "date": "2021-09-08", "amount": "4273.82", "category": "Stock Sold"}
+		]
+	}, {
+		"date": "2021-09-07",
+		"I": 297.64,
+		"O": 480.17,
+		"transactions": [
+			{"isIn": true, "date": "2021-09-07", "amount": "297.64", "category": "Stock Sold"},
+			{"isIn": false, "date": "2021-09-07", "amount": "480.17", "category": "Dentist"}
+		]
+	}
 ]
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
-  }
+const server = http.createServer(function (request, response) {
+	if (request.method === 'GET') {
+		handleGet(request, response)
+	} else if (request.method === 'POST') {
+		handlePost(request, response)
+	}
 })
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
-
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
-  }
+const handleGet = function (request, response) {
+	const filename = dir + request.url.slice(1)
+	if (request.url === '/') {
+		sendFile(response, 'public/index.html')
+	} else if (request.url === '/submit') {
+		console.log(request)
+		console.log(response)
+		console.log(filename)
+	} else {
+		sendFile(response, filename)
+	}
 }
 
-const handlePost = function( request, response ) {
-  let dataString = ''
+const handlePost = function (request, response) {
+	let dataString = ''
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
+	request.on('data', function (data) {
+		dataString += data
+	})
 
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
-
-    // ... do something with the data here!!!
-
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
-  })
+	request.on('end', function () {
+		let received = JSON.parse(dataString)
+		console.log(received)
+		if (received.type === 'getData') {
+			response.writeHead(200, "OK", {'Content-Type': 'text/plain'})
+			response.end(JSON.stringify(appdata))
+		} else if (received.type === "getBudget") {
+			response.writeHead(200, "OK", {'Content-Type': 'text/plain'})
+			response.end(JSON.stringify(budget))
+		} else if (received.type === "budget") {
+			budget = received.budget
+			console.log(budget)
+			response.writeHead(200, "OK", {'Content-Type': 'text/plain'})
+			response.end(JSON.stringify(received.budget))
+		} else if ((received.type === "income" || received.type === "expense") && !isNaN(Date.parse(received.date)) && !isNaN(received.amount)) {
+			let searched = appdata.find(each => each.date === received.date)
+			if (searched !== undefined) {
+				if (received.type === "expense") {
+					searched.transactions.push(
+						{
+							"isIn": false,
+							"date": received.date,
+							"amount": received.amount,
+							"category": received.category
+						})
+					searched.O += +received.amount
+				} else {
+					searched.transactions.push(
+						{
+							"isIn": true,
+							"date": received.date,
+							"amount": received.amount,
+							"category": received.category
+						})
+					searched.I += +received.amount
+				}
+			} else if (received.type === "expense") {
+				appdata.push(
+					{
+						"date": received.date,
+						"I": 0.00,
+						"O": +received.amount,
+						"transactions": [
+							{
+								"isIn": false,
+								"date": received.date,
+								"amount": received.amount,
+								"category": received.category
+							}
+						]
+					})
+			} else {
+				appdata.push(
+					{
+						"date": received.date,
+						"I": +received.amount,
+						"O": 0.00,
+						"transactions": [
+							{
+								"isIn": true,
+								"date": received.date,
+								"amount": received.amount,
+								"category": received.category
+							}
+						]
+					})
+			}
+			// console.log(appdata)
+			// appdata.forEach(each => each.transactions.forEach(each => console.log(each)))
+			response.writeHead(200, "OK", {'Content-Type': 'text/plain'})
+			response.end(JSON.stringify(appdata))
+		} else {
+			console.log(appdata)
+			response.writeHead(200, "OK", {'Content-Type': 'text/plain'})
+			response.end("Illegal action, not performed")
+		}
+	})
 }
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+const sendFile = function (response, filename) {
+	const type = mime.getType(filename)
 
-   fs.readFile( filename, function( err, content ) {
+	fs.readFile(filename, function (err, content) {
 
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
-
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
-
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
+		// if requesting a teapot, then we return a teapot
+		// if the error = null, then we've loaded the file successfully
+		if (filename === 'public/teapot') {
+			response.writeHeader(418)
+			response.end(`418: I'm a teapot`)
+		} else if (err === null) {
+			response.writeHeader(200, {'Content-Type': type})
+			response.end(content)
+			console.log('Accessed: ' + filename)
+		} else {
+			response.writeHeader(404)
+			response.end('404 Error: File Not Found')
+		}
+	})
 }
 
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port)
