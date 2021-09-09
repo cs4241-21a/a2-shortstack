db39a067da93b648b668a47641d55f70d21f50ed
lewasserman <laurenewasserman@gmail.com>
2021-09-09 02:47:32 -0400

    Derived Field

--- server.improved.js
+++ server.improved.js
@@ -7,8 +7,11 @@ const http = require( 'http' ),
       port = 3000
 
 var appdata = [
-  { 'title': 'Animal Farm', 'author': 'George Orwell', 'year': 2003, 'rating': 3, 'rank': 2 },
-  { 'title': 'The Sun Also Rises', 'author': ' Ernest Hemingway', 'year': 1957, 'rating': 4, 'rank': 1 }
+  { 'title': 'Animal Farm', 'author': 'George Orwell', 'year': 2003, 'rating': 2 },
+  { 'title': 'The Sun Also Rises', 'author': 'Ernest Hemingway', 'year': 1957, 'rating': 4 },
+  { 'title': 'Title 3', 'author': 'Author 3', 'year': 2020, 'rating': 2 },
+  { 'title': 'Title 1', 'author': 'Author 1', 'year': 2000, 'rating': 4 },
+  { 'title': 'Title 2', 'author': 'Author 2', 'year': 1900, 'rating': 5 }
 ]
 
 const server = http.createServer( function( request,response ) {
@@ -42,6 +45,24 @@ const handlePost = function( request, response ) {
     json.rank = 4
     // appdata = {...appdata, ...json}
     appdata.push(json)
+    appdata.sort(function ( a, b ) {
+      if ( a.rating < b.rating ){
+        return 1;
+      }
+      if ( a.rating > b.rating ){
+        return -1;
+      }
+      if( a.year < b.year){
+        return 1;
+      }
+      if ( a.year > b.year){
+        return -1;
+      }
+      return 0;
+    })
+    for(var i = 0; i < appdata.length; i++){
+      appdata[i].rank = i+1
+    }
 
     // ... do something with the data here!!!
 
