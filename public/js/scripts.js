86a32cccf8b1ee1302656b9ffcf4442ef6ffde1d
DanStus <dstusalitus@wpi.edu>
2021-09-09 10:29:40 -0400

    DId the whole prokect lol

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,157 @@
 // Add some Javascript code here, to run on the front end.
 
 console.log("Welcome to assignment 2!")
+
+let editFlag = -1
+
+const submit = function( e ) {
+    // prevent default form action from being carried out
+    e.preventDefault()
+
+    const input1 = document.querySelector( '#descrip' ),
+          input2 = document.querySelector( '#expectedTime' ),
+          input3 = document.querySelector( '#dueDate' ),
+          prompt = document.querySelector("#prompt"),
+          json = { descrip: input1.value,
+                   expectedTime: input2.value,
+                   dueDate: input3.value },
+          body = editFlag != -1 ? "EDIT" + editFlag + JSON.stringify( json ) : JSON.stringify( json )
+
+
+    editFlag = -1
+
+    fetch( '/submit', {
+      method:'POST',
+      body 
+    })
+    .then( response => response.json() )
+    .then( function ( json ) {
+      // do something with the reponse
+      const element = document.querySelector('#taskTable')
+      
+      for(let i = 1; i <= json.length; i++){
+        if(i < element.rows.length){
+          element.rows[i].cells[0].innerHTML = json[i-1].descrip
+          element.rows[i].cells[1].innerHTML = json[i-1].expectedTime
+          element.rows[i].cells[2].innerHTML = json[i-1].dueDate
+          element.rows[i].cells[3].innerHTML = json[i-1].DueDate
+        } else {
+          let newRow = element.insertRow(i)
+
+          newRow.insertCell(0)
+          newRow.insertCell(1)
+          newRow.insertCell(2)
+          newRow.insertCell(3)
+
+          newRow.cells[0].innerHTML = json[i-1].descrip
+          newRow.cells[1].innerHTML = json[i-1].expectedTime
+          newRow.cells[2].innerHTML = json[i-1].dueDate
+          newRow.cells[3].innerHTML = json[i-1].DueDate
+
+          let editBtn = document.createElement("button")
+          editBtn.classList.add("tableBtn")
+          editBtn.classList.add("edit")
+          editBtn.innerHTML = "Edit"
+          editBtn.onclick = function () {
+            input1.value = newRow.cells[0].innerHTML
+            input2.value = newRow.cells[1].innerHTML
+            input3.value = newRow.cells[2].innerHTML
+            prompt.innerHTML = "Enter new data for this row"
+            editFlag = i;
+          }
+          let dltBtn = document.createElement("button")
+          dltBtn.classList.add("tableBtn")
+          dltBtn.classList.add("delete")
+          dltBtn.innerHTML = "Delete"
+          dltBtn.onclick = function () {
+            let body = "DELETE" + i
+            fetch( '/submit', {
+              method:'POST',
+              body 
+            })
+            element.deleteRow(i)
+          }
+          newRow.appendChild(editBtn)
+          newRow.appendChild(dltBtn)
+        }
+      }
+
+      // element.innerText = json[0].descrip
+      // document.body.appendChild( element )
+      // console.log(JSON.stringify(json))
+    })
+
+    prompt.innerHTML = "Enter a task"
+
+    return false
+  }
+
+  window.onload = function() {
+    const button = document.querySelector( 'button' )
+    if(button !== null)
+      button.onclick = submit
+
+    let body = "GETDATA"
+    fetch( '/submit', {
+      method:'POST',
+      body 
+    })
+    .then( response => response.json() )
+    .then( function ( json ) {
+      // do something with the reponse
+      const element = document.querySelector('#taskTable')
+      
+      for(let i = 1; i <= json.length; i++){
+        if(i < element.rows.length){
+          element.rows[i].cells[0].innerHTML = json[i-1].descrip
+          element.rows[i].cells[1].innerHTML = json[i-1].expectedTime
+          element.rows[i].cells[2].innerHTML = json[i-1].dueDate
+          element.rows[i].cells[3].innerHTML = json[i-1].DueDate
+        } else {
+          let newRow = element.insertRow(i)
+
+          newRow.insertCell(0)
+          newRow.insertCell(1)
+          newRow.insertCell(2)
+          newRow.insertCell(3)
+
+          newRow.cells[0].innerHTML = json[i-1].descrip
+          newRow.cells[1].innerHTML = json[i-1].expectedTime
+          newRow.cells[2].innerHTML = json[i-1].dueDate
+          newRow.cells[3].innerHTML = json[i-1].DueDate
+
+          if(document.title === "CS4241 Assignment 2"){
+            let editBtn = document.createElement("button")
+            editBtn.classList.add("tableBtn")
+            editBtn.classList.add("edit")
+            editBtn.innerHTML = "Edit"
+            editBtn.onclick = function () {
+              input1.value = newRow.cells[0].innerHTML
+              input2.value = newRow.cells[1].innerHTML
+              input3.value = newRow.cells[2].innerHTML
+              prompt.innerHTML = "Enter new data for this row"
+              editFlag = i;
+            }
+            let dltBtn = document.createElement("button")
+            dltBtn.classList.add("tableBtn")
+            dltBtn.classList.add("delete")
+            dltBtn.innerHTML = "Delete"
+            dltBtn.onclick = function () {
+              let body = "DELETE" + i
+              fetch( '/submit', {
+                method:'POST',
+                body 
+              })
+              element.deleteRow(i)
+            }
+            newRow.appendChild(editBtn)
+            newRow.appendChild(dltBtn)
+          }
+        }
+      }
+
+      // element.innerText = json[0].descrip
+      // document.body.appendChild( element )
+      // console.log(JSON.stringify(json))
+    })
+  }
--- server.improved.js
+++ server.improved.js
@@ -1,3 +1,5 @@
+const { getDefaultSettings } = require('http2')
+
 const http = require( 'http' ),
       fs   = require( 'fs' ),
       mime = require( 'mime' ),
@@ -5,10 +7,9 @@ const http = require( 'http' ),
       port = 3000
 
 //Sample appdata, replace this with something else as needed
+//This is an array holding objects, which is what I want to do for my data until I
+//potentially switch to having a persistent database
 const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
 ]
 
 const server = http.createServer( function( request,response ) {
@@ -37,12 +38,25 @@ const handlePost = function( request, response ) {
   })
 
   request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
 
-    // ... do something with the data here!!!
+    if(dataString.slice(0,4) === "EDIT"){
+      let rowNum = parseInt(dataString.slice(4,5))
+      dataString = dataString.slice(5)
+      appdata[rowNum-1] = JSON.parse( dataString )
+    } else if(dataString.slice(0,6) === "DELETE"){
+      let rowNum = parseInt(dataString.slice(6,7))
+      appdata.splice(rowNum - 1, 1)
+    } else if (dataString === "GETDATA"){
+        //Do nothing here
+    } else {
+      appdata.push(JSON.parse( dataString ))
+    }
+
+    appdata.forEach( element => timeUntilDue(element))
 
     response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
+    //JSON.stringify(appdata[appdata.length-1])
+    response.end(JSON.stringify( appdata ))
   })
 }
 
@@ -66,6 +80,45 @@ const sendFile = function( response, filename ) {
 
      }
    })
+}
+
+const timeUntilDue = function(data){
+  let start = new Date()
+          year = start.getFullYear()
+          month = start.getMonth() + 1
+          date = start.getDate()
+          hour = start.getHours()
+          minute = start.getMinutes()
+          dueYear = parseInt(data.dueDate.slice(0, 4))
+          dueMonth = parseInt(data.dueDate.slice(5, 7))
+          dueDay = parseInt(data.dueDate.slice(8, 10))
+          dueHour = parseInt(data.dueDate.slice(11, 13))
+          dueMinute = parseInt(data.dueDate.slice(14, 16))
+
+
+  let timeLeft = (((((dueYear - year) * 12 +  dueMonth - month) * 30 + dueDay - date) * 24 + dueHour - hour) * 60 + dueMinute - minute)
+  
+  if(timeLeft < 0){
+    data.DueDate = "Overdue!"
+  } else {
+
+      let minutesLeft = timeLeft % 60
+          hoursLeft = Math.floor(timeLeft / 60) % 24
+          daysLeft = Math.floor(timeLeft / 60 / 24) % 30
+          monthsLeft = Math.floor(timeLeft / 60 / 24 / 30) % 12
+          yearsLeft =  Math.floor(timeLeft / 60 / 24 / 30 / 12)
+          dueString = "Due in "
+
+      dueString += yearsLeft > 0 ? yearsLeft + " years " : ""
+      dueString += monthsLeft > 0 ? monthsLeft + " months " : ""
+      dueString += daysLeft > 0 ? daysLeft + " days " : ""
+      dueString += hoursLeft > 0 ? hoursLeft + " hours " : ""
+      dueString += minutesLeft > 0 ? minutesLeft + " minutes" : ""
+
+      
+    data.DueDate = dueString
+  }
+  
 }
 
 server.listen( process.env.PORT || port )
