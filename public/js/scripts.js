8a1aa13615c012692ec98f75bdc2073d2f912b4e
mcaten <maddiecaten@gmail.com>
2021-09-08 21:39:30 -0400

    submit button function

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,8 +2,12 @@ const submit = function( e ) {
   // prevent default form action from being carried out
   e.preventDefault()
 
-  const t = document.querySelector( '#title' ),
-        json = { title: t.value },
+  const title = document.querySelector( '#title' ),
+    author = document.querySelector(' #author '),
+    score = document.querySelector(' #score '),
+    notes = document.querySelector(' #notes '),
+    year = document.querySelector(' #year' ),
+        json = { title: title.value, author: author.value, score: Number(score.value), notes: notes.value, year: Number(year.value)},
         body = JSON.stringify( json )
 
   fetch( '/submit', {
@@ -13,6 +17,9 @@ const submit = function( e ) {
   .then( function( response ) {
     // do something with the reponse 
     console.log( response )
+  })
+  .then( function( json ) {
+    document.querySelector('form').reset();
   })
 
   return false
