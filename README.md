7112c64e267b8e188fb89a7d06e1314c6a7aaa75
Glitch (a2-trbouwens) <none>
2021-09-09 14:39:12 +0000

    🌴👂 Updated with Glitch

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Tyler Bouwens - Tyler's RPG Dice Roller
 My site is a database for TTRPG (Table Top Role Playing Game) dice rolling, the standard Dice pack has 7 dice in it, those being 4-sided, 6 sided, 8 sided, two 10 sided, a 12 sided, and a 20 sided dice. I omitted one of said 10 sided dice, which is used for percentages, in favour of the more fun 100 sided d10. The site allows you to specify your or your character's name, the type of dice to roll, the quantity of dice to roll, and a modifier to add to the final result. The table will dynamically display the rolls of everyone using it, and has the ability to delete individual rolls, or clear it all-together.
 
-## Technical Achievements
+### Technical Achievements
 - **Tech Achievement 1**: Using a function to rewrite table data, the table of data always stays live updated with current information. Thanks to the onload functionality, it also loads the data to the table on loading of the site itself.
 - **Tech Achievement 2**: The table data can be deleted, either in part or in whole. I opted not to have a modify functionality, as I figured that went against the spirit of dice-rolling. Table data can be deleted either by specifying one character to delete all entries for, or by roll ID.
 - **Tech Achievement 3**: Data in the table can be sorted on Character, and there's a button under clear to do just that.
--- server.improved.js
+++ server.improved.js
@@ -1,92 +1,86 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require("http"),
+  fs = require("fs"),
+  // IMPORTANT: you must run `npm install` in the directory for this assignment
+  // to install the mime library used in the following line of code
+  mime = require("mime"),
+  dir = "public/",
+  port = 3000;
 
-let appdata = []
+let appdata = [];
 
-let dataCount = 0
-let id = 0
+let dataCount = 0;
+let id = 0;
 
-const rollDice = function( type, quantity, mod) {
-  totalRoll = 0
+let error = "error";
 
-  for (i = 0; i < parseInt(quantity,10); i++) {
-    let baseRoll = Math.floor(Math.random() * parseInt(type,10));
-    totalRoll += (baseRoll + 1)
+const rollDice = function(type, quantity, mod) {
+  let totalRoll = 0;
+
+  for (let i = 0; i < parseInt(quantity, 10); i++) {
+    let baseRoll = Math.floor(Math.random() * parseInt(type, 10));
+    totalRoll += baseRoll + 1;
   }
-  return(totalRoll + parseInt(mod,10))
-}
+  return totalRoll + parseInt(mod, 10);
+};
 
-
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )
+const server = http.createServer(function(request, response) {
+  if (request.method === "GET") {
+    handleGet(request, response);
+  } else if (request.method === "POST") {
+    handlePost(request, response);
   }
-  else if( request.method === 'POST' ){
-    handlePost( request, response )
-  }
-})
+});
 
 //
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 )
+const handleGet = function(request, response) {
+  const filename = dir + request.url.slice(1);
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
+  if (request.url === "/") {
+    sendFile(response, "public/index.html");
+  } else if (request.url === "/clear") {
+    clearRolls(response);
+  } else if (request.url === "/sort") {
+    sortRolls(response);
+  } else if (request.url === "/load") {
+    sendTable(response);
+  } else {
+    sendFile(response, filename);
   }
-  else if ( request.url === "/clear") {
-    clearRolls(response)
-  }
-  else if ( request.url === "/sort") {
-    sortRolls(response)
-  }
-  else if (request.url === "/load") {
-    sendTable(response)
-  }
-  else{
-    sendFile( response, filename )
-  }
-}
+};
 
 //
-const handlePost = function( request, response ) {
-  let dataString = ''
+const handlePost = function(request, response) {
+  let dataString = "";
 
-  request.on( 'data', function( data ) {
-      dataString += data
-  })
+  request.on("data", function(data) {
+    dataString += data;
+  });
 
-  request.on( 'end', function() {
-    let data = JSON.parse(dataString)
+  request.on("end", function() {
+    let data = JSON.parse(dataString);
 
-    if(request.url === "/add") {
-      if(addRoll(data)) {
-        sendTable(response)
+    if (request.url === "/add") {
+      if (addRoll(data)) {
+        sendTable(response);
+      } else {
+        response.writeHead(419, "MESSAGE", { "Content-Type": "application/json" });
+        response.end(error);
       }
-      else{
-        response.writeHead(400, "Add failed", {'Content-Type': 'text/plain'});
-        response.end();
-      }
-    }
-    else if(request.url === "/delete") {
-      if(deleteRoll(data)) {
-        sendTable(response)
-      }
-      else{
-        response.writeHead(400, "Delete failed", {'Content-Type': 'text/plain'});
-        response.end();
+    } else if (request.url === "/delete") {
+      if (deleteRoll(data)) {
+        sendTable(response);
+      } else {
+        response.writeHead(419, error, {
+          "Content-Type": "text/plain"
+        });
+        response.end(error);
       }
     }
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
-  })
-}
-
+    response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+    response.end();
+  });
+};
 
 //
 const addRoll = function(data) {
@@ -96,34 +90,39 @@ const addRoll = function(data) {
     quantity must not be: NaN, <= 0
     modifier must not be: NaN, < 0
   */
-  if (data.quantity <= 0 || data.modifier < 0 ||
-      isNaN(data.quantity) || isNaN(data.modifier) ||
-      data.character.length == 0) {
-
-      return false
+  if (
+    data.quantity <= 0 ||
+    data.modifier < 0 ||
+    isNaN(data.quantity) ||
+    isNaN(data.modifier) ||
+    data.character.length == 0
+  ) 
+  {
+    error = "Please fill all required entries!"
+    return false;
   }
 
-  thisRoll = rollDice(data.diceType,data.quantity,data.modifier)
+  let thisRoll = rollDice(data.diceType, data.quantity, data.modifier);
 
-  json = {
-      "id" : id,
-      "character" : data.character,
-      "diceType" : data.diceType,
-      "quantity" : data.quantity,
-      "modifier" : data.modifier,
-      "roll" : thisRoll
-  }
+  let json = {
+    id: id,
+    character: data.character,
+    diceType: data.diceType,
+    quantity: data.quantity,
+    modifier: data.modifier,
+    roll: thisRoll
+  };
 
-  appdata.push(json)
+  appdata.push(json);
 
-  id++
-  dataCount++
+  id++;
+  dataCount++;
 
-  console.log("TO ADD:")
-  console.log(json)
+  console.log("TO ADD:");
+  console.log(json);
 
-  return true
-}
+  return true;
+};
 
 //
 const deleteRoll = function(data) {
@@ -134,113 +133,110 @@ const deleteRoll = function(data) {
     Charater must not be: "" if ID is none.
   */
   if (data.id < 0 || data.id > id || isNaN(data.id) || data.id.length == 0) {
-    console.log("DELETE VIA CHAR NAME")
 
-    if(data.character.length == 0) {
-      console.log("Here>")
-      return false
-    }
+    if (data.character.length == 0) {
+      error = "One of ID or Character must be filled out."
+      return false;
+    } 
     else {
       // Delete via character instead.
-      newList = []
-      newCount = dataCount
-      for(let j = 0; j < dataCount; j++) {
-        console.log(appdata[j].character + " === " + data.character)
-        if(appdata[j].character === data.character) {
-          console.log("TO DELETE:")
-          console.log(appdata[j])
-          newCount--
-          continue
+      let newList = [];
+      let newCount = dataCount;
+      for (let j = 0; j < dataCount; j++) {
+        console.log(appdata[j].character + " === " + data.character);
+        if (appdata[j].character === data.character) {
+          console.log("TO DELETE:");
+          console.log(appdata[j]);
+          newCount--;
+          continue;
         }
-        newList.push(appdata[j])
+        newList.push(appdata[j]);
       }
-      appdata = newList
-      dataCount = newCount
+      appdata = newList;
+      dataCount = newCount;
 
-      return true
+      return true;
     }
   }
 
   // Loop Through the data to find correct row.
-  let index = -1
-  for(let i = 0; i < dataCount; i++) {
-      console.log(appdata[i].id + " == " + data.id)
-      if (appdata[i].id == data.id) {
-        index = i
-        break
-      }
+  let index = -1;
+  for (let i = 0; i < dataCount; i++) {
+    console.log(appdata[i].id + " == " + data.id);
+    if (appdata[i].id == data.id) {
+      index = i;
+      break;
+    }
   }
 
-  if(index === -1) {
-    return false
+  if (index === -1) {
+    return false;
   }
 
-  appdata.splice(index, 1)
-  dataCount--
+  appdata.splice(index, 1);
+  dataCount--;
 
-  console.log("TO DELETE:")
-  console.log(appdata[index])
+  console.log("TO DELETE:");
+  console.log(appdata[index]);
 
-  return true
-
-}
+  return true;
+};
 
 //
 const clearRolls = function(response) {
-  appdata = []
-  dataCount = 0
-  id = 0
-  sendTable(response)
-}
+  appdata = [];
+  dataCount = 0;
+  id = 0;
+  sendTable(response);
+};
 
 const sortRolls = function(response) {
   // Thanks Stack Overflow for helping me on this one.
   // https://stackoverflow.com/questions/6712034/sort-array-by-firstname-alphabetically-in-javascript
-  appdata.sort( function(a, b) {
-      if(a.character < b.character) { return -1; }
-      if(a.character > b.character) { return 1; }
-      return 0;
-  })
-  sendTable(response)
-}
+  appdata.sort(function(a, b) {
+    if (a.character < b.character) {
+      return -1;
+    }
+    if (a.character > b.character) {
+      return 1;
+    }
+    return 0;
+  });
+  sendTable(response);
+};
 
 //
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename )
+const sendFile = function(response, filename) {
+  const type = mime.getType(filename);
 
-   fs.readFile( filename, function( err, content ) {
-
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
-
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
-
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
-}
+  fs.readFile(filename, function(err, content) {
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { "Content-Type": type });
+      response.end(content);
+    } else {
+      // file not found, error code 404
+      response.writeHeader(404);
+      response.end("404 Error: File Not Found");
+    }
+  });
+};
 
 //
 const sendTable = function(response) {
-    let json = {
-        "nRows": dataCount,
-        "rowData": []
-    }
+  let json = {
+    nRows: dataCount,
+    rowData: []
+  };
 
-    for(let i = 0; i < dataCount; i++){
-        json["rowData"].push(appdata[i]);
-    }
+  for (let i = 0; i < dataCount; i++) {
+    json["rowData"].push(appdata[i]);
+  }
 
-    let body = JSON.stringify(json);
-    response.writeHead(200, "OK", {"Content-Type": "text/plain"});
-    response.end(body);
-}
+  let body = JSON.stringify(json);
+  response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+  response.end(body);
+};
 
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port);
