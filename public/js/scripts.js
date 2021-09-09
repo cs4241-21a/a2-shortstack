dcfbce9593626854c72a8d91e5f4194f9231d910
Jasper Meggitt <jasper.meggitt@gmail.com>
2021-09-09 10:48:30 -0400

    Do primary tasks in assignment

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,105 @@
-// Add some Javascript code here, to run on the front end.
 
-console.log("Welcome to assignment 2!")
+
+function updateLeaderboard(values) {
+    const elements = Object.entries(values)
+        .sort(([, a], [, b]) => b.score - a.score)
+        .map(([id, x], index) => `<tr id="value${id}">
+                                    <td>${index + 1}</td>
+                                    <td>${x.username}</td>
+                                    <td>${x.score}</td>
+                                    <td>${x.date}</td>
+                                    <td>
+                                        <button type="button" class="edit">Edit</button>
+                                        <button type="button" class="remove">Remove</button>
+                                    </td>
+                                  </tr>`)
+        .join("\n");
+
+    const table = document.querySelector("#leaderboard")
+    table.tBodies[0].innerHTML = table.rows[0].innerHTML
+    table.tBodies[0].innerHTML += elements
+
+    document.querySelectorAll(".remove").forEach(x => x.onclick = removeButton)
+    document.querySelectorAll(".edit").forEach(x => x.onclick = editButton)
+}
+
+function removeButton(e) {
+    // Not necessary, but do anyway just to be safe
+    e.preventDefault()
+
+    const targetID = e.path[2].id;
+    const id = parseInt(targetID.substring(5))
+    console.log("Removing row of id " + id)
+
+    const body = JSON.stringify({
+        "id": id,
+    })
+
+    fetch('/api/remove', {
+        method: 'POST',
+        body
+    })
+        .then(x => x.json())
+        .then(updateLeaderboard)
+
+    return false
+}
+
+function editButton(e) {
+    if (e.path[0].innerHTML === "Edit") {
+        e.path[0].innerHTML = "Save"
+
+        console.log(e.path)
+        const user = e.path[2].children[1]
+        const score = e.path[2].children[2]
+
+        user.innerHTML = `<input value="${user.innerHTML}">`
+        score.innerHTML = `<input value="${score.innerHTML}">`
+    } else {
+        e.path[0].innerHTML = "Edit"
+
+        const body = JSON.stringify({
+            "id": parseInt(e.path[2].id.substring(5)),
+            "username": e.path[2].children[1].children[0].value,
+            "score": parseInt(e.path[2].children[2].children[0].value),
+        })
+
+        fetch('/api/edit', {
+            method: 'POST',
+            body
+        })
+            .then(x => x.json())
+            .then(updateLeaderboard)
+    }
+
+    return false
+}
+
+function fetchValues() {
+    fetch('/api/values', {method: 'GET'})
+        .then(x => x.json())
+        .then(updateLeaderboard)
+}
+
+
+function addUser(e) {
+    // prevent default form action from being carried out
+    e.preventDefault()
+
+    const username = document.querySelector('#username')
+    const score = document.querySelector('#score')
+
+    const body = JSON.stringify({
+        "username": username.value,
+        "score": score.value,
+    })
+
+    fetch('/api/add', {
+        method: 'POST',
+        body
+    })
+        .then(x => x.json())
+        .then(updateLeaderboard)
+
+    return false
+}
--- server.improved.js
+++ server.improved.js
@@ -1,72 +1,192 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require('http'),
+    fs = require('fs'),
+    // IMPORTANT: you must run `npm install` in the directory for this assignment
+    // to install the mime library used in the following line of code
+    mime = require('mime'),
+    dir = 'public/',
+    port = 3000
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+let dataCounter = 6;
+const appdata = {
+    0: {"rank": 1, "username": "Dave23", "score": 342, "date": new Date().toISOString()},
+    3: {"rank": 2, "username": "Legend69", "score": 235, "date": new Date().toISOString()},
+    5: {"rank": 3, "username": "bob", "score": 111, "date": new Date().toISOString()},
+    4: {"rank": 4, "username": "Dave23", "score": 29, "date": new Date().toISOString()},
+}
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
-  }
+
+function apiGetValues(request, response) {
+    response.writeHead(200, "OK", {'Content-Type': 'text/json'})
+    response.end(JSON.stringify(appdata))
+}
+
+function collectPost(request, callback) {
+    let dataString = ''
+    request.on('data', data => dataString += data)
+    request.on('end', () => callback(dataString))
+}
+
+function apiPostAddition(request, response) {
+    collectPost(request, function (data) {
+        const field = JSON.parse(data);
+
+        if (!(field.hasOwnProperty("username") && field.hasOwnProperty("score"))) {
+            response.writeHead(400, "Bad Request")
+            response.end("Missing username and/or score from request")
+            return
+        }
+
+        if (!(typeof field.score === "number" && (field.score | 0) === field.score)) {
+            response.writeHead(400, "Bad Request")
+            response.end("Score must be an integer value")
+            return
+        }
+
+        if (!(typeof field.username === "string" && field.username.length > 0)) {
+            response.writeHead(400, "Bad Request")
+            response.end("Username must be a non-empty string value")
+            return
+        }
+
+        appdata[dataCounter] = {
+            "username": field.username,
+            "score": field.score,
+            "date": new Date().toISOString(),
+        }
+
+        dataCounter += 1;
+        apiGetValues(request, response)
+    })
+}
+
+function apiPostRemove(request, response) {
+    collectPost(request, function (data) {
+        const field = JSON.parse(data);
+
+        if (!(field.hasOwnProperty("id"))) {
+            response.writeHead(400, "Bad Request")
+            response.end("Missing id from request")
+            return
+        }
+
+        if (!appdata.hasOwnProperty(field.id)) {
+            response.writeHead(400, "Bad Request")
+            response.end("Row ID not found")
+            return
+        }
+
+        delete appdata[field.id]
+        apiGetValues(request, response)
+    })
+}
+
+function apiPostEdit(request, response) {
+    collectPost(request, function (data) {
+        const field = JSON.parse(data);
+
+        if (!(field.hasOwnProperty("id") && field.hasOwnProperty("username") && field.hasOwnProperty("score"))) {
+            response.writeHead(400, "Bad Request")
+            response.end("Missing username, score and/or id from request")
+            return
+        }
+
+        if (!(typeof field.score === "number" && (field.score | 0) === field.score)) {
+            response.writeHead(400, "Bad Request")
+            response.end("Score must be an integer value")
+            return
+        }
+
+        if (!(typeof field.username === "string" && field.username.length > 0)) {
+            response.writeHead(400, "Bad Request")
+            response.end("Username must be a non-empty string value")
+            return
+        }
+
+        if (!appdata.hasOwnProperty(field.id)) {
+            response.writeHead(400, "Bad Request")
+            response.end("Row ID not found")
+            return
+        }
+
+        appdata[field.id] = {
+            "username": field.username,
+            "score": field.score,
+            "date": new Date().toISOString(),
+        }
+        apiGetValues(request, response)
+    })
+}
+
+const handleGet = function (request, response) {
+    if (request.url === '/') {
+        sendFile(response, 'public/index.html')
+    } else {
+        sendFile(response, "public" + request.url)
+    }
+}
+
+const sendFile = function (response, filename) {
+    const type = mime.getType(filename)
+
+    fs.readFile(filename, function (err, content) {
+
+        // if the error = null, then we've loaded the file successfully
+        if (err === null) {
+
+            // status code: https://httpstatuses.com
+            response.writeHeader(200, {'Content-Type': type})
+            response.end(content)
+
+        } else {
+            // file not found, error code 404
+            response.writeHeader(404)
+            response.end('404 Error: File Not Found')
+        }
+    })
+}
+
+function unknownRoute(request, response) {
+    response.writeHead(400, "Bad Request")
+    response.end("Unknown route: " + request.method + ": " + request.url)
+}
+
+const routes = {
+    "GET": {
+        "/api/values": apiGetValues,
+        "/api": unknownRoute,
+        "/": handleGet,
+    },
+    "POST": {
+        "/api/add": apiPostAddition,
+        "/api/remove": apiPostRemove,
+        "/api/edit": apiPostEdit,
+    }
+}
+
+const server = http.createServer(function (request, response) {
+    if (!routes.hasOwnProperty(request.method)) {
+        unknownRoute(request, response)
+        return
+    }
+
+    for (const [route, fn] of Object.entries(routes[request.method])) {
+        if (request.url.startsWith(route)) {
+            fn(request, response)
+            return
+        }
+    }
+
+    unknownRoute(request, response)
+
+    // if (request.url.startsWith("/api")) {
+    //     handleAPI(request, response)
+    // } else {
+    //     if (request.method === 'GET') {
+    //         handleGet(request, response)
+    //     } else if (request.method === 'POST') {
+    //         handlePost(request, response)
+    //     }
+    // }
 })
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
-
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
-  }
-}
-
-const handlePost = function( request, response ) {
-  let dataString = ''
-
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
-
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
-
-    // ... do something with the data here!!!
-
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
-  })
-}
-
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
-
-   fs.readFile( filename, function( err, content ) {
-
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
-
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
-
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
-}
-
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port)
