640f9dda384d2446f71b2cc074dab8cf655b4371
lplopez-wpi <56080197+lplopez-wpi@users.noreply.github.com>
2021-09-09 02:34:50 -0400

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,45 +1,43 @@
 // Add some Javascript code here, to run on the front end.
 
-//let table = document.getElementById("streamingTable");
+//let table = document.getElementById("watchlist");
 let clientdata = []
 
-function updateCollections(data){
+function updateTable(data){
   let table = document.getElementById("streamingTable");
   let json_data = JSON.parse(data);
-//   clientdata = json_data["data"];
- table.innerHTML = "<tr> <th>Streaming Service</th><th>Currently Subscribed</th> <th>Name</th> <th>Watch Again (yes or no)?</th><th>Rating (1-10)</th><th>Recommendation (yes or no)?</th><th>Our verdit:</th></tr>"
- json_data.forEach(element=> addMedia(element["streamingServiceItem"], element["subscriptionItem"], element["nameOfItem"], element["watchAgainItem"], element["ratingItem"], element["recommendItem"], element["theVerdict"]))
- // addMedia(json_data.title, json_data.director, json_data.)
+ table.innerHTML = "<tr> <th>Streaming Service</th><th>Subscription Status</th> <th>Name</th> <th>Watch Again</th><th>Rating Item</th><th>Recommendation</th></tr>"
+ json_data.forEach(element=> addMedia(element["streamingServiceItem"], element["subscriptionItem"], element["nameOfItem"], element["watchAgainItem"], element["ratingItem"], element["recommendation"]))
 }
 
 
-function addMedia(streamingService,subscribed,name,watchAgain,rating, recommend, theVerdict){
-  console.log(streamingService,subscribed,name,watchAgain,rating, recommend)
+function addMedia(streamingService,subscriptionStatus,name,watchAgain,rating,recommendation){
+  console.log(streamingService,subscriptionStatus,name,watchAgain,rating)
   let table = document.getElementById("streamingTable");
 
   let row = table.insertRow(-1);
-
+  let theaterChecker = document.getElementById("watchAgain");
+  
   let cell0 = row.insertCell(0);
   let cell1 = row.insertCell(1);
   let cell2 = row.insertCell(2);
   let cell3 = row.insertCell(3);
   let cell4 = row.insertCell(4); 
   let cell5 = row.insertCell(5);
-  let cell6 = row.insertCell(6);
-
+  
   cell0.innerHTML = streamingService;
-  cell1.innerHTML = subscribed;
+  cell1.innerHTML = subscriptionStatus;
   cell2.innerHTML = name;
   cell3.innerHTML = watchAgain;
-  cell4.innerHTML = rating;
-  cell5.innerHTML = recommend;
-
-  if (watchAgain && recommend) {
-      cell6.innerHTML = "I'm lovin it!";
-  } else cell6.innerHTML = "I'm gonna let that slide";
-
-  cell6.innerHTML = theVerdict; 
-
+  cell4.innerlHTML = rating;
+  
+  
+  if (watchAgain.parsedInfo("yes")) {
+      cell3.innerHTML = "Yes";
+  } else cell3.innerHTML = "No";
+  
+  cell5.innerHTML = recommendation; 
+  
 }
 
-console.log("Welcome to Assignment #2! I had fun with Master Yoda and Master Oogway, if you couldn't already tell!")
+console.log("Welcome to assignment 2!")
