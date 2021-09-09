3dc1755d53453614561fb64ba9956c04e27ad404
KyleWynnM <74930526+KyleWynnM@users.noreply.github.com>
2021-09-09 01:36:26 -0400

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,48 @@
-// Add some Javascript code here, to run on the front end.
 
-console.log("Welcome to assignment 2!")
+  const submit = function( e ) {
+    // prevent default form action from being carried out
+    e.preventDefault()
+
+    /**const input = document.querySelector( '#yourname' ),
+          json = { yourname: input.value },
+          body = JSON.stringify( json )**/
+
+    let websiteName = document.querySelector("#websiteName")
+    let username = document.querySelector("#username")
+    let pw = document.querySelector("#pw")
+    let json = {
+      websiteName: websiteName.value,
+      username: username.value,
+      pw: pw.value,
+      critique: ""}
+    let body = JSON.stringify(json)
+    fetch( '/submit', {
+      method:'POST',
+      body
+    })
+    .then( async function ( response ) {
+      let newData = await response.json()
+      refreshInfo(newData)
+      console.log( newData )
+    })
+
+    return false
+  }
+
+  function refreshInfo(newData) {
+    const board = document.getElementById("heldData")
+    board.innerHTML = ""
+
+    newData.forEach((element, index) => {
+        board.innerHTML +=
+          "<tr><td>" + element.websiteName + "</td><td>"
+          + element.username + "</td><td>"
+          + element.pw + "</td><td>"
+          + element.critique + "</td></tr>"
+      })
+  }
+
+  window.onload = function() {
+    const button = document.querySelector( 'button' )
+    button.onclick = submit
+  }
