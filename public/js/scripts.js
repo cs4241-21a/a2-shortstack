3644865133f06d3f002b19cfdef9275ad17a4d25
jake Feiss <jakefeiss@jakes-Mac-mini.local>
2021-09-09 00:46:47 -0400

    all the code

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,115 @@
 // Add some Javascript code here, to run on the front end.
 
-console.log("Welcome to assignment 2!")
+let dataArr = []
+
+const submit = function( e ) {
+    
+    let name = document.querySelector( '#yourname' )
+
+    if (name.value ==="") {
+        alert("Fields not filled in")
+        return false
+    } 
+
+
+          json = { yourname: name.value }
+          body = JSON.stringify( json )
+
+    fetch( '/submit', {
+      method:'POST',
+      body 
+    })
+    .then( function( response ) {
+      return response.text()
+    })
+
+    .then( function ( text ) {
+        dataArr.push(JSON.parse(text));
+        updateTable();
+        console.log("dataArr: " + JSON.stringify(dataArr));
+    })
+
+    return false
+  }
+
+
+
+  const modify = function (e) {
+
+      let entry = dataArr[Number(e.target.id.substring(1))];
+
+      document.querySelector('#yourname').value = entry.yourname
+
+      dataArr.splice(Number(e.target.id.substring(1)), 1);
+      console.log("Current dataArr when editing: " + JSON.stringify(dataArr))
+
+      updateTable();
+    }
+
+  const remove = function (e) {
+    e.preventDefault()
+
+    //Gets the id of the row that needs to be deleted
+    dataArr.splice(Number(e.target.id.substring(3)), 1);
+    console.log("Current dataArr after deletion: " + JSON.stringify(dataArr))
+
+    updateTable();
+  }
+
+  window.onload = function() {
+    const button = document.querySelector( 'button' )
+    button.onclick = submit
+  }
+
+  function updateTable() {
+
+    let tbody = document.querySelector('tbody')
+    tbody.innerHTML = ""
+
+    for (let index = 0; index < dataArr.length; index++) {
+
+        let newRow = document.createElement('tr')
+
+        for (let cell = 0; cell < 6; cell++) {
+
+            let newCell = document.createElement('td')
+            let newText;
+
+           switch(cell) {
+               case 0:
+                   //Creates a new cell that contains the Name value
+                   newText = document.createTextNode(dataArr[index].yourname);
+                   break;
+                case 1:
+                    //Creates a new cell that contains the Edit button
+                    newText = document.createElement('Input')
+                    newText.setAttribute('type','button')
+                    newText.setAttribute('value', 'Edit')
+                    newText.className = 'editButton';
+                    break;
+                case 2:
+                    //Creates a new cell that contains the Remove button
+                    newText = document.createElement('Input')
+                    newText.setAttribute('type','button')
+                    newText.setAttribute('value', 'Remove')
+                    newText.className = 'removeButton';
+                    break;
+           }
+            //Adds an event to the Edit button
+            if (cell === 1) {
+                newText.id = "Edit" + index.toString()
+                newText.onclick = modify;
+            }
+            //Adds an event to the Remove button
+            if (cell === 2) {
+                newText.id = "Delete" + index.toString()
+                newText.onclick = remove;
+            }
+            //Placing each text to there appropriate cell and placing each cell to a row
+            newCell.appendChild(newText)
+            newRow.appendChild(newCell)
+        }
+        //Placing the row in the tbody
+        tbody.appendChild(newRow)
+    }
+  }
--- server.improved.js
+++ server.improved.js
@@ -6,12 +6,14 @@ const http = require( 'http' ),
       dir  = 'public/',
       port = 3000
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+let appdata=[
+   {yourname:'Jake', password:'password',age:'20', birthyear:'2001'},
+   {yourname:'Harrison', password:'RocketLeague4Life',age:'20', birthyear:'2001'},
+   {yourname:'Loren', password:'Lily',age:'20', birthyear:'2001'},
+   {yourname:'Jake', password:'ManteTeo',age:'21', birthyear:'2000'},
+   {yourname:'Jakes GF', password:'noOneKnows',age:'?', birthyear:'null'}
 
+]
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
     handleGet( request, response )    
@@ -34,17 +36,81 @@ const handlePost = function( request, response ) {
   let dataString = ''
 
   request.on( 'data', function( data ) {
-      dataString += data 
+      dataString += data
   })
 
   request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+ 
+  if( request.url === '/submit' ) {
+       //console.log( JSON.parse( dataString ) )
+
+    // ... do something with the data here!!!
+    const json = JSON.parse(dataString)
+let age = json.age
+        let birthyear = 2021-age
+      json.birthyear=birthyear
+
+      //console.log("json: " + JSON.stringify(json))
+      
+      //appdata=json
+      appdata.push(json)
+      //appdata=[{'1':'1', '2':'2', '3':'3'}]
+//appdata=[{yourname:'dad', password:'blu22', age:'20', birthyear:'2001'}]
+      //console.log(appdata)
+    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+    response.end(JSON.stringify(appdata))
+  }
+  if( request.url === '/remove' ) {
+     //console.log(dataString.substring(6))
+      appdata.splice(dataString.substring(6),1)
+      //appdata=appdata.filter(function(el){
+      //  return el.del!=dataString
+      //})
+      //console.log(appdata)
+     response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+    response.end(JSON.stringify(appdata))
+  
+  }
+    if( request.url === '/load' ) {
+       //console.log( JSON.parse( dataString ) )
 
     // ... do something with the data here!!!
 
+      //console.log("json: " + JSON.stringify(json))
+      
+      //appdata=json
+      //appdata.push(json)
+      //appdata=[{'1':'1', '2':'2', '3':'3'}]
+//appdata=[{yourname:'dad', password:'blu22', age:'20', birthyear:'2001'}]
+      //console.log(appdata)
     response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
-  })
+    response.end(JSON.stringify(appdata))
+  }
+     if( request.url === '/modify' ) {
+       //console.log( JSON.parse( dataString ) )
+
+    // ... do something with the data here!!!
+
+      //console.log("json: " + JSON.stringify(json))
+     let json2 = dataString.substring(0,dataString.indexOf('}')+1)
+     let index = dataString.substring(dataString.indexOf('}')+5)
+    //console.log(json2, index)
+json2 = JSON.parse(json2)
+        let age = json2.age
+        let birthyear = 2021-age
+      json2.birthyear=birthyear
+
+
+         
+      //appdata=json
+      appdata[index]=json2
+      //appdata=[{'1':'1', '2':'2', '3':'3'}]
+//appdata=[{yourname:'dad', password:'blu22', age:'20', birthyear:'2001'}]
+      //console.log(appdata)
+    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+    response.end(JSON.stringify(appdata))
+  }
+ })
 }
 
 const sendFile = function( response, filename ) {
@@ -60,6 +126,7 @@ const sendFile = function( response, filename ) {
        response.end( content )
 
      }else{
+
 
        // file not found, error code 404
        response.writeHeader( 404 )
