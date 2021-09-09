f224872cf1d3815a14bb6d361fae91d7777bc829
Glitch (a2-bennyrob) <none>
2021-09-09 15:22:37 +0000

    🎑🙏 Updated with Glitch

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,24 @@
-// Add some Javascript code here, to run on the front end.
+         function updateTable(name, responsibility, watch, fan){
+            let table = document.getElementById("responses");
 
-console.log("Welcome to assignment 2!")
+            let n = row.insertCell(0);
+            let r = row.insertCell(1);
+            let w = row.insertCell(2);
+            let f = row.insertCell(3);
+          
+            
+            n.innerHTML = name.replace("+"," ");
+            r.innerHTML = responsibility;
+            if(watch == "y"){
+                w.innerHTML = "Yes";
+            } else {
+                w.innerHTML = "No";
+            }
+          
+            if(fan == "y"){
+                f.innerHTML = "Yes";
+            } else {
+                f.innerHTML = "No";
+            }
+
+        }
